c9b4b06e819711515f74b6a544603d512f76a7a5
pludemann <pludemann@google.com>
2015-03-24 10:00:27 -0700

    Test workflow doc examples. (Example doesn't yet work, so it's skipped.) ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=89409489

---
+++ pytype/tests/test_workflow.py
@@ -0,0 +1,32 @@
+"""Test cases that match the workflow doc."""
+
+import unittest
+from pytype.tests import test_inference
+
+
+class WorkflowTest(test_inference.InferenceTest):
+
+  @unittest.skip("Needs fix for with/return and must output attributes")
+  def testWorkflow1(self):
+    with self.Infer("""
+      class ConfigParser:
+        def __init__(self, filename):
+          self.filename = filename
+        def read(self):
+          with open(self.filename, "r") as fi:
+            return fi.read()
+
+      cp = ConfigParser(__any_object__())
+      cp.read()
+      """, deep=False, solve_unknowns=True, extract_locals=False) as ty:
+      self.assertTypesMatchPytd(ty, """
+        class ConfigParser:
+          # TODO(pludemann): remove '-> NoneType'
+          def __init__(self, filename: str or buffer or unicode) -> NoneType
+          def read(self) -> str
+          filename: str or buffer or unicode
+      """)
+
+
+if __name__ == '__main__':
+  test_inference.main(debugging=False)
