dbf5d049e7963a857ed4fb95b6b33340aaa92b74
kramm <kramm@google.com>
2015-05-01 17:56:06 -0700

    Re-enable pytree test. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=92600104

--- pytype/tests/test_pytree.py
+++ pytype/tests/test_pytree.py
@@ -6,14 +6,13 @@ from pytype.pytd import utils
 from pytype.tests import test_inference
 
 
-@unittest.skip("TODO(kramm): Flaky.")
 class PyTreeTests(test_inference.InferenceTest):
 
   def testDeep(self):
     sourcecode = utils.GetDataFile("examples/pytree.py")
     # TODO(pludemann): use new 'with' framework
     # TODO(pludemann): extract_locals - see class test_inference.Infer
-    ty = self._InferAndVerify(sourcecode, deep=True, expensive=False)
+    ty = self._InferAndVerify(sourcecode, deep=True)
     function_names = {f.name for f in ty.functions}
     class_names = {cls.name for cls in ty.classes}
     self.assertIn("type_repr", function_names)
