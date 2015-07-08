8d9d6c90d6123022277b819a5537a2b16f0c63df
rechen <rechen@google.com>
2015-07-08 13:10:47 -0700

    Use self.Infer in test_pytree. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=97805147

--- pytype/tests/test_pytree.py
+++ pytype/tests/test_pytree.py
@@ -10,14 +10,13 @@ class PyTreeTests(test_inference.InferenceTest):
 
   def testDeep(self):
     sourcecode = pytd_utils.GetDataFile("examples/pytree.py")
-    # TODO(pludemann): use new 'with' framework
     # TODO(pludemann): extract_locals - see class test_inference.Infer
-    ty = self._InferAndVerify(sourcecode, deep=True)
-    function_names = {f.name for f in ty.functions}
-    class_names = {cls.name for cls in ty.classes}
-    self.assertIn("type_repr", function_names)
-    self.assertIn("Node", class_names)
-    self.assertIn("Leaf", class_names)
+    with self.Infer(sourcecode, deep=True) as ty:
+      function_names = {f.name for f in ty.functions}
+      class_names = {cls.name for cls in ty.classes}
+      self.assertIn("type_repr", function_names)
+      self.assertIn("Node", class_names)
+      self.assertIn("Leaf", class_names)
 
 
 if __name__ == "__main__":
