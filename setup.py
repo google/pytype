19beec68ac5eb10386a0a18e6b66b3453418d512
Matthias Kramm <kramm@google.com>
2016-01-13 08:45:49 -0800

    make setup.py install typeshed

--- setup.py
+++ setup.py
@@ -2,6 +2,23 @@
 
 from distutils.core import setup
 
+import glob
+import os
+
+
+def scan_package_data(path, pattern):
+    result = []
+    for subdir, _, _, in os.walk(path):
+        full_pattern = os.path.join(subdir, pattern)
+        if glob.glob(full_pattern):
+          result.append(full_pattern)
+    return result
+
+
+typeshed = scan_package_data('typeshed', '*.pyi')
+assert 'typeshed/stdlib/2.7/*.pyi' in typeshed
+
+
 setup(
     name='pytype',
     version='0.2',
@@ -9,9 +26,15 @@ setup(
     maintainer='Google',
     maintainer_email='pytypedecl-dev@googlegroups.com',
     url='http://github.com/google/pytype',
-    packages=['pytype', 'pytype/pyc', 'pytype/pytd', 'pytype/pytd/parse'],
+    packages=['pytype',
+              'pytype/pyc',
+              'pytype/pytd',
+              'pytype/pytd/parse',
+             ],
     scripts=['scripts/pytype', 'scripts/pytd'],
-    package_data={'pytype': ['pytd/builtins/*']},
+    package_data={'pytype': ['pytd/builtins/*',
+                             'pytd/stdlib/*',
+                            ] + typeshed},
     requires=['ply (>=3.4)'],
     install_requires=['ply>=3.4'],
 )
