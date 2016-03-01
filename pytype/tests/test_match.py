2d086e60fd69ce1e6ea1d6877a39024d318e1ae9
kramm <kramm@google.com>
2016-02-29 16:08:42 -0800

    Allow functions to match against "Callable".
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=115917163

--- pytype/tests/test_match.py
+++ pytype/tests/test_match.py
@@ -4,57 +4,19 @@ from pytype.tests import test_inference
 
 
 class MatchTest(test_inference.InferenceTest):
-  """Tests for matching types."""
 
   def testCallable(self):
-    ty = self.Infer("""
+    with self.Infer("""
       import tokenize
       def f():
         pass
       x = tokenize.generate_tokens(f)
-    """, deep=True, solve_unknowns=False, extract_locals=True)
-    self.assertTypesMatchPytd(ty, """
-      tokenize = ...  # type: module
-      def f() -> NoneType
-      x = ...  # type: Generator[Tuple[Union[Tuple[int, ...], int, str], ...]]
-    """)
-
-  def testBoundAgainstCallable(self):
-    ty = self.Infer("""
-      import tokenize
-      import StringIO
-      x = tokenize.generate_tokens(StringIO.StringIO("").readline)
-    """, deep=True, solve_unknowns=False, extract_locals=True)
-    self.assertTypesMatchPytd(ty, """
-      tokenize = ...  # type: module
-      StringIO = ...  # type: module
-      x = ...  # type: Generator[Tuple[Union[Tuple[int, ...], int, str], ...]]
-    """)
-
-  def testMatchUnknownAgainstContainer(self):
-    ty = self.Infer("""
-      a = {1}
-      def f(x):
-        return a & x
-    """, deep=True, solve_unknowns=True)
-    self.assertTypesMatchPytd(ty, """
-      a = ...  # type: Set[int]
-
-      def f(x: set) -> set: ...
-    """)
-
-  def testMatchStatic(self):
-    ty = self.Infer("""
-      s = {1}
-      def f(x):
-        # set.intersection is a static method:
-        return s.intersection(x)
-    """, deep=True, solve_unknowns=True)
-    self.assertTypesMatchPytd(ty, """
-      s = ...  # type: Set[int]
-
-      def f(x) -> set: ...
-    """)
+    """, deep=True, solve_unknowns=False, extract_locals=True) as ty:
+      self.assertTypesMatchPytd(ty, """
+        tokenize = ...  # type: module
+        def f() -> NoneType
+        x = ...  # type: Generator[Tuple[Any, ...]]
+      """)
 
 
 if __name__ == "__main__":
