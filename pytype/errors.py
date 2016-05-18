7d4c834b8b61e82082dc2c4d0204db6db1a7c6cc
kramm <kramm@google.com>
2016-05-18 13:23:56 -0700

    Make import-error mention '.pyi'.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=122661636

--- pytype/errors.py
+++ pytype/errors.py
@@ -205,12 +205,8 @@ class ErrorLog(ErrorLogBase):
 
   @_error_name("import-error")
   def import_error(self, opcode, module_name):
-    self.error(opcode, "Can't find module %r." % module_name)
-
-  @_error_name("import-error")
-  def import_from_error(self, opcode, module, name):
-    module_name = module.data[0].name
-    self.error(opcode, "Can't find %s.%s" % (module_name, name))
+    self.error(opcode, "Can't find module %r.\n" % module_name +
+               "Did you generate a .pyi?")
 
   @_error_name("wrong-arg-count")
   def wrong_arg_count(self, opcode, sig, call_arg_count):
