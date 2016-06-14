8b0aa9ac434587640351c63f4ecc9395ed390589
acaceres <acaceres@google.com>
2016-06-14 12:35:44 -0700

    Look for __init__.pyi files when loading a module stub from typeshed. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=124869372

--- pytype/pytd/typeshed.py
+++ pytype/pytd/typeshed.py
@@ -21,7 +21,7 @@ def get_typeshed_file(toplevel, module, version, typeshed_dir=None):
       directly under pytype (i.e., /{some_path}/pytype/typeshed).
 
   Returns:
-    The contents of the file
+    A tuple with the filename and contents of the file
   Raises:
     IOError: if file not found
   """
@@ -37,7 +37,7 @@ def get_typeshed_file(toplevel, module, version, typeshed_dir=None):
     # https://github.com/python/typeshed/pull/42
     assert toplevel == "builtins"
     raise IOError("No directory %s" % prefix)
-  filename = os.path.join(*module.split(".")) + ".pyi"
+  module_path = os.path.join(*module.split("."))
   versions = ["%d.%d" % (version[0], minor)
               for minor in range(version[1], -1, -1)]
   # E.g. for Python 3.5, try 3.5/, 3.4/, 3.3/, ..., 3.0/, 3/, 2and3.
@@ -45,16 +45,18 @@ def get_typeshed_file(toplevel, module, version, typeshed_dir=None):
   # The order is the same as that of mypy. See default_lib_path in
   # https://github.com/JukkaL/mypy/blob/master/mypy/build.py#L249
   for v in versions + [str(version[0]), "2and3"]:
-    path = os.path.join(prefix, v, filename)
-    if loader and typeshed_dir is None:
-      # PEP 302 loader API
-      data = loader.get_data(path)  # See pytd.data_files.GetPredefinedFile
-      if data:
-        return data
-    if os.path.isfile(path):
-      with open(path, "rb") as fi:
-        return fi.read()
-  raise IOError("Couldn't find %s" % filename)
+    path_base = os.path.join(prefix, v, module_path)
+    for path in [os.path.join(path_base, "__init__.pyi"), path_base + ".pyi"]:
+      if loader and typeshed_dir is None:
+        # PEP 302 loader API
+        data = loader.get_data(path)  # See pytd.data_files.GetPredefinedFile
+        if data:
+          return path, data
+      if os.path.isfile(path):
+        with open(path, "rb") as fi:
+          return path, fi.read()
+
+  raise IOError("Couldn't find %s" % module)
 
 
 def parse_type_definition(pyi_subdir, module, python_version):
@@ -69,9 +71,8 @@ def parse_type_definition(pyi_subdir, module, python_version):
     The AST of the module; None if the module doesn't have a definition.
   """
   try:
-    src = get_typeshed_file(pyi_subdir, module, python_version)
+    filename, src = get_typeshed_file(pyi_subdir, module, python_version)
   except IOError:
     return None
-  name = os.path.join(str(python_version), module + ".pyi")  # for debugging
-  return utils.ParsePyTD(src, filename=name, module=module,
+  return utils.ParsePyTD(src, filename=filename, module=module,
                          python_version=python_version).Replace(name=module)
--- pytype/pytd/typeshed_test.py
+++ pytype/pytd/typeshed_test.py
@@ -1,6 +1,7 @@
 """Tests for typeshed.py."""
 
 import unittest
+import os
 
 
 from pytype.pytd import typeshed
@@ -11,8 +12,14 @@ class TestTypeshed(parser_test_base.ParserTest):
   """Test the code for loading files from typeshed."""
 
   def test_get_typeshed_file(self):
-    data = typeshed.get_typeshed_file("stdlib", "errno", (2, 7))
+    filename, data = typeshed.get_typeshed_file("stdlib", "errno", (2, 7))
+    self.assertEqual("errno.pyi", os.path.basename(filename))
     self.assertIn("errorcode", data)
+
+  def test_get_typeshed_dir(self):
+    filename, data = typeshed.get_typeshed_file("stdlib", "logging", (2, 7))
+    self.assertEqual("__init__.pyi", os.path.basename(filename))
+    self.assertIn("LogRecord", data)
 
   def test_parse_type_definition(self):
     ast = typeshed.parse_type_definition("stdlib", "_random", (2, 7))
