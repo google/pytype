49314914edad89e1c6b8a1fcea2a9c4a5c064fc7
kramm <kramm@google.com>
2016-06-28 11:26:22 -0700

    Improve sharding of typeshed tests. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=126096908

--- pytype/pytd/typeshed_test.py
+++ pytype/pytd/typeshed_test.py
@@ -2,7 +2,6 @@
 
 import os
 import re
-import unittest
 
 
 from pytype.pytd import typeshed
@@ -29,7 +28,31 @@ class TestTypeshedLoading(parser_test_base.ParserTest):
     self.assertIn("_random.Random", [cls.name for cls in ast.classes])
 
 
-class _TestTypeshedParsing(parser_test_base.ParserTest):
+def _walk_dir(path):
+  for root, _, filenames in os.walk(path):
+    for f in filenames:
+      yield os.path.join(root, f)
+
+
+def _filename_to_testname(f):
+  return "test" + os.path.splitext(os.path.basename(f))[0]
+
+
+def _test_parse(pyi_file):
+  python_version = (2, 7)
+  module = os.path.splitext(os.path.basename(pyi_file))[0]
+  if "__init__" == module:
+    module = os.path.basename(os.path.dirname(pyi_file))
+  with open(pyi_file) as f:
+    src = f.read()
+  # Call ParsePyTD directly to avoid typeshed.get_typeshed_file logic
+  utils.ParsePyTD(src,
+                  filename=pyi_file,
+                  module=module,
+                  python_version=python_version)
+
+
+class TestTypeshedParsing(parser_test_base.ParserTest):
   """Test that we can parse a given pyi file."""
   # Files that we currently can't parse
   SKIPPED_FILES = """
@@ -76,49 +99,19 @@ class _TestTypeshedParsing(parser_test_base.ParserTest):
       2and3/math.pyi
       2and3/webbrowser.pyi
   """
+  WANTED = re.compile(r"stdlib/(2\.7|2and3)/.*\.pyi$")
+  SKIPPED = re.compile("(%s)$" % "|".join(SKIPPED_FILES.split()))
+  TYPESHED_DIR = typeshed.get_typeshed_dir()
 
-  def __init__(self, pyi_file):
-    super(_TestTypeshedParsing, self).__init__("run_test")
-    self.pyi_file = pyi_file
-
-  def run_test(self):
-    python_version = (2, 7)
-    module = os.path.splitext(os.path.basename(self.pyi_file))[0]
-    if "__init__" == module:
-      module = os.path.basename(os.path.dirname(self.pyi_file))
-
-    with open(self.pyi_file) as f:
-      src = f.read()
-
-    # Call ParsePyTD directly to avoid typeshed.get_typeshed_file logic
-    utils.ParsePyTD(src,
-                    filename=self.pyi_file,
-                    module=module,
-                    python_version=python_version)
-
-
-def _walk_dir(path):
-  for root, _, filenames in os.walk(path):
-    for f in filenames:
-      yield os.path.join(root, f)
-
-
-def load_tests(unused_loader, standard_tests, unused_pattern):
-  # Only testing against part of typeshed
-  wanted = re.compile(r"stdlib/(2\.7|2and3)/.*\.pyi$")
-  skipped = re.compile("(%s)$" % "|".join(
-      _TestTypeshedParsing.SKIPPED_FILES.split()))
-
-  found_tests = False
-  typeshed_dir = typeshed.get_typeshed_dir()
-  suite = unittest.unittest.TestSuite(standard_tests)
-  for f in _walk_dir(typeshed_dir):
-    if wanted.search(f) and not skipped.search(f):
-      suite.addTest(_TestTypeshedParsing(f))
-      found_tests = True
-
-  assert found_tests
-  return suite
+  # Generate test methods
+  # pylint: disable=no-self-argument,g-wrong-blank-lines,undefined-loop-variable
+  for f in _walk_dir(TYPESHED_DIR):
+    if WANTED.search(f) and not SKIPPED.search(f):
+      def _bind(f):
+        return lambda self: _test_parse(f)
+      locals()[_filename_to_testname(f)] = _bind(f)
+  del _bind
+  del f
 
 
 if __name__ == "__main__":
