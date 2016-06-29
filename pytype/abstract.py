b5221c18a4af433409ac62e952aa7bf6590527c0
rechen <rechen@google.com>
2016-06-29 09:20:07 -0700

    Fix arguments passed to __get__. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=126198959

--- pytype/abstract.py
+++ pytype/abstract.py
@@ -253,6 +253,9 @@ class AtomicAbstractValue(object):
     """
     return node, None
 
+  def get_attribute_generic(self, node, name, val):
+    return self.get_attribute(node, name, valself=val)
+
   def get_attribute(self, node, name, valself=None, valcls=None,
                     condition=None):
     """Get the named attribute from this object.
@@ -1680,6 +1683,9 @@ class PyTDClass(SimpleAbstractValue, Class):
     self.pytd_cls = pytd_cls
     self.mro = utils.compute_mro(self)
 
+  def get_attribute_generic(self, node, name, val):
+    return self.get_attribute(node, name, valcls=val)
+
   def get_attribute(self, node, name, valself=None, valcls=None,
                     condition=None):
     return Class.get_attribute(self, node, name, valself, valcls, condition)
@@ -1815,6 +1821,9 @@ class InterpreterClass(SimpleAbstractValue, Class):
   def get_attribute_flat(self, node, name):
     return SimpleAbstractValue.get_attribute(self, node, name)
 
+  def get_attribute_generic(self, node, name, val):
+    return self.get_attribute(node, name, valcls=val)
+
   def get_attribute(self, node, name, valself=None, valcls=None,
                     condition=None):
     node, attr_var = Class.get_attribute(self, node, name, valself, valcls,
@@ -1826,8 +1835,14 @@ class InterpreterClass(SimpleAbstractValue, Class):
       value = v.data
       node2, getter = value.get_attribute(node, "__get__", v)
       if getter is not None:
-        node2, get_result = self.vm.call_function(
-            node2, getter, [getter, value.get_class()])
+        posargs = []
+        if valself:
+          posargs.append(valself.variable)
+        if valcls:
+          if not valself:
+            posargs.append(self.vm.none.to_variable(node, "None"))
+          posargs.append(valcls.variable)
+        node2, get_result = self.vm.call_function(node2, getter, posargs)
         for getter in get_result.bindings:
           result.AddBinding(getter.data, [getter], node2)
       else:
