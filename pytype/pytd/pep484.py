bd55bc897b4d2a6457431bc0c04699f4d69f58ff
rechen <rechen@google.com>
2016-08-04 17:33:32 -0700

    Attempt 2! Define AnyStr as a TypeVar.
    Defines AnyStr as a TypeVar in typing.pytd and imports it wherever AnyStr
    is used (rather than replacing it with Union[str, unicode]).
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=129393853

--- pytype/pytd/pep484.py
+++ pytype/pytd/pep484.py
@@ -7,7 +7,7 @@ from pytype.pytd import pytd
 from pytype.pytd.parse import visitors
 
 
-PEP484_NAMES = ["AbstractSet", "BinaryIO", "ByteString", "Callable",
+PEP484_NAMES = ["AbstractSet", "AnyStr", "BinaryIO", "ByteString", "Callable",
                 "Container", "Dict", "Final", "FrozenSet", "Generator",
                 "Generic", "Hashable", "IO", "ItemsView", "Iterable",
                 "Iterator", "KeysView", "List", "Mapping", "MappingView",
@@ -24,8 +24,6 @@ PEP484_TRANSLATIONS = {
     "None": pytd.NamedType("NoneType"),
     # PEP 484 definitions of special purpose types:
     "Any": pytd.AnythingType(),
-    "AnyStr": pytd.UnionType([pytd.NamedType("str"),
-                              pytd.NamedType("unicode")]),
     # TODO(kramm): "typing.NamedTuple"
 }
 
@@ -195,15 +193,8 @@ class ConvertTypingToNative(visitors.Visitor):
     if module == "typing":
       if name in visitors.PrintVisitor.PEP484_CAPITALIZED:
         return pytd.NamedType(name.lower())  # "typing.List" -> "list" etc.
-      elif name == "Any":
-        return pytd.AnythingType()
-      elif name == "AnyStr":
-        if self.python_version[0] >= 3:
-          return pytd.UnionType((pytd.NamedType("bytes"),
-                                 pytd.NamedType("str")))
-        else:
-          return pytd.UnionType((pytd.NamedType("str"),
-                                 pytd.NamedType("unicode")))
+      elif name in PEP484_TRANSLATIONS:
+        return PEP484_TRANSLATIONS[name]
       else:
         # IO, Callable, etc. (I.e., names in typing we leave alone)
         return t
