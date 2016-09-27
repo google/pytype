647fa5f995c73ec79d348e572bc914de67b52ed4
Rebecca Chen <rechen@google.com>
2016-09-27 00:12:40 +0000

    Update test_data/complex.py
    After the change to DEEP_VARIABLE_LIMIT, the previous program wasn't
    complex enough for pytype to fail.
    
    --
    MOS_MIGRATED_REVID=134347007

--- pytype/test_data/complex.py
+++ pytype/test_data/complex.py
@@ -1,6 +1,8 @@
 """Too-complex program."""
 
 
+w = [0, 0.1, 1j, "foo", slice(0, 3), u"foo", None, type, max]
 x = [0, 0.1, 1j, "foo", slice(0, 3), u"foo", None, type, max]
 y = [0, 0.1, 1j, "foo", slice(0, 3), u"foo", None, type, max]
-zip(x, y)
+z = [0, 0.1, 1j, "foo", slice(0, 3), u"foo", None, type, max]
+zip(w, x, y, z)
