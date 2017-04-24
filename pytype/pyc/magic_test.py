760399144a7dc6b9bd9784caf639c2c674e80a89
Martin DeMello <mdemello@google.com>
2017-04-24 20:14:02 +0000

    Add new magic numbers from python 3.5 and 3.6
    --
    PiperOrigin-RevId: 154086984
    MOS_MIGRATED_REVID=154086984

--- pytype/pyc/magic_test.py
+++ pytype/pyc/magic_test.py
@@ -11,6 +11,8 @@ class TestMagic(unittest.TestCase):
   def test_to_version(self):
     self.assertEquals(magic.magic_word_to_version('\x03\xf3'), (2, 7))
     self.assertEquals(magic.magic_word_to_version('\xee\x0c'), (3, 4))
+    self.assertEquals(magic.magic_word_to_version('\x17\x0d'), (3, 5))
+    self.assertEquals(magic.magic_word_to_version('\x33\x0d'), (3, 6))
 
 if __name__ == '__main__':
   unittest.main()
