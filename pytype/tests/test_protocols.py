0b9b36451758e1accdf188c32fa850d76d2c7f7b
Paige Kordas <kordas@google.com>
2017-06-20 17:23:30 +0000

    Unskipped tests that are already passing
    --
    PiperOrigin-RevId: 159577807
    MOS_MIGRATED_REVID=159577807

--- pytype/tests/test_protocols.py
+++ pytype/tests/test_protocols.py
@@ -12,8 +12,8 @@ from pytype.tests import test_inference
 class ProtocolTest(test_inference.InferenceTest):
   """Tests for protocol implementation."""
 
-  @unittest.skip("Moving to protocols.")
   def test_multiple_signatures_with_type_parameter(self):
+    self.options.tweak(protocols=True)
     with utils.Tempdir() as d:
       d.create_file("foo.pyi", """
         from typing import List, TypeVar
@@ -25,14 +25,14 @@ class ProtocolTest(test_inference.InferenceTest):
         import foo
         def f(x, y):
           return foo.f(x, y)
-      """, pythonpath=[d.path], deep=True, solve_unknowns=True)
+      """, pythonpath=[d.path], deep=True)
       self.assertTypesMatchPytd(ty, """
         foo = ...  # type: module
         def f(x, y: int or str) -> list
       """)
 
-  @unittest.skip("Moving to protocols.")
   def test_unknown_single_signature(self):
+    self.options.tweak(protocols=True)
     # Test that the right signature is picked in the presence of an unknown
     with utils.Tempdir() as d:
       d.create_file("foo.pyi", """
@@ -45,15 +45,15 @@ class ProtocolTest(test_inference.InferenceTest):
         import foo
         def f(y):
           return foo.f("", y)
-      """, pythonpath=[d.path], deep=True, solve_unknowns=True)
+      """, pythonpath=[d.path], deep=True)
       self.assertTypesMatchPytd(ty, """
         from typing import List
         foo = ...  # type: module
         def f(y: int) -> List[str]
       """)
 
-  @unittest.skip("Moving to protocols.")
   def test_multiple_signatures_with_unknown(self):
+    self.options.tweak(protocols=True)
     with utils.Tempdir() as d:
       d.create_file("foo.pyi", """
         def f(arg1: str) -> float
@@ -63,14 +63,14 @@ class ProtocolTest(test_inference.InferenceTest):
         import foo
         def f(x):
           return foo.f(x)
-      """, pythonpath=[d.path], deep=True, solve_unknowns=True)
+      """, pythonpath=[d.path], deep=True)
       self.assertTypesMatchPytd(ty, """
         foo = ...  # type: module
         def f(x: int or str) -> float or bool
       """)
 
-  @unittest.skip("Moving to protocols.")
   def test_multiple_signatures_with_optional_arg(self):
+    self.options.tweak(protocols=True)
     with utils.Tempdir() as d:
       d.create_file("foo.pyi", """
         def f(x: str) -> int
@@ -80,14 +80,14 @@ class ProtocolTest(test_inference.InferenceTest):
         import foo
         def f(x):
           return foo.f(x)
-      """, pythonpath=[d.path], deep=True, solve_unknowns=True)
+      """, pythonpath=[d.path], deep=True)
       self.assertTypesMatchPytd(ty, """
         foo = ...  # type: module
         def f(x: str) -> int or float
       """)
 
-  @unittest.skip("Moving to protocols.")
   def test_multiple_signatures_with_kwarg(self):
+    self.options.tweak(protocols=True)
     with utils.Tempdir() as d:
       d.create_file("foo.pyi", """
         def f(*, y: int) -> bool
@@ -97,33 +97,34 @@ class ProtocolTest(test_inference.InferenceTest):
         import foo
         def f(x):
           return foo.f(y=x)
-      """, pythonpath=[d.path], deep=True, solve_unknowns=True)
+      """, pythonpath=[d.path], deep=True)
       self.assertTypesMatchPytd(ty, """
         foo = ...  # type: module
         def f(x: int or str) -> bool or float
       """)
 
-  @unittest.skip("Moving to protocols.")
   def testPow2(self):
+    self.options.tweak(protocols=True)
     ty = self.Infer("""
       def t_testPow2(x, y):
         # pow(int, int) returns int, or float if the exponent is negative.
         # Hence, it's a handy function for testing UnionType returns.
         return pow(x, y)
-    """, deep=True, solve_unknowns=True)
+    """, deep=True)
     self.assertTypesMatchPytd(ty, """
       def t_testPow2(x: complex or float or int, y: complex or float or int) -> complex or float or int
     """)
 
   @unittest.skip("Moving to protocols.")
   def testSlices(self):
+    self.options.tweak(protocols=True)
     ty = self.Infer("""
       def trim(docstring):
         lines = docstring.splitlines()
         for line in lines[1:]:
           len(line)
         return lines
-    """, deep=True, solve_unknowns=True)
+    """, deep=True)
     self.assertTypesMatchPytd(ty, """
       from typing import List
       def trim(docstring: bytearray or str or unicode) -> List[bytearray or str or unicode, ...]
@@ -131,11 +132,12 @@ class ProtocolTest(test_inference.InferenceTest):
 
   @unittest.skip("Moving to protocols.")
   def testMatchUnknownAgainstContainer(self):
+    self.options.tweak(protocols=True)
     ty = self.Infer("""
       a = {1}
       def f(x):
         return a & x
-    """, deep=True, solve_unknowns=True)
+    """, deep=True)
     self.assertTypesMatchPytd(ty, """
       from typing import Iterable, Set
       a = ...  # type: Set[int]
@@ -143,8 +145,8 @@ class ProtocolTest(test_inference.InferenceTest):
       def f(x: Iterable) -> Set[int]: ...
     """)
 
-  @unittest.skip("Moving to protocols.")
   def testWorkflow1(self):
+    self.options.tweak(protocols=True)
     ty = self.Infer("""
       class ConfigParser(object):
         def __init__(self, filename):
@@ -155,7 +157,7 @@ class ProtocolTest(test_inference.InferenceTest):
 
       cp = ConfigParser(__any_object__())
       cp.read()
-      """, deep=False, solve_unknowns=True)
+      """, deep=False)
     self.assertTypesMatchPytd(ty, """
       cp = ...  # type: ConfigParser
 
