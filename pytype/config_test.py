a4e8a1edb49483f4d1c8a170dbac880a5a76de90
Martin DeMello <mdemello@google.com>
2018-04-06 21:39:54 +0000

    Add a small config test, assert 2-and-3 compatibility.
    --
    PiperOrigin-RevId: 191942209
    MOS_MIGRATED_REVID=191942209

---
+++ pytype/config_test.py
@@ -0,0 +1,30 @@
+"""Tests for config.py."""
+
+
+from pytype import config
+from pytype.tests import test_base
+
+import unittest
+
+
+class ConfigTest(unittest.TestCase):
+
+  def test_basic(self):
+    argv = [
+        "pytype",
+        "-V", "3.6",
+        "--use-pickled-files",
+        "-o", "out.pyi",
+        "--pythonpath", "foo:bar",
+        "test.py"
+    ]
+    opts = config.Options(argv)
+    self.assertEqual(opts.python_version, (3, 6))
+    self.assertEqual(opts.use_pickled_files, True)
+    self.assertEqual(opts.pythonpath, ["foo", "bar"])
+    self.assertEqual(opts.output, "out.pyi")
+    self.assertEqual(opts.input, "test.py")
+
+
+if __name__ == "__main__":
+  test_base.main()
