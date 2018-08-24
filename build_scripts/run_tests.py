20395e1fe0a79255fcc236ecad3d4a5ea3ec5e3e
Siva Chandra <sivachandra@google.com>
2018-08-24 11:42:24 -0700

    Adjust run_tests.py to reflect the fact that it also runs C++ unit tests.

--- build_scripts/run_tests.py
+++ build_scripts/run_tests.py
@@ -3,10 +3,10 @@
 
 Usage:
 
-$> python run_tests.py [MODULE] [MODULE] ...
+$> python run_tests.py [TARGET] [TARGET] ...
 
-A MODULE is a fully qualified name of a test module within the PyType
-source tree. If no module is specified, all test targets listed in the
+A TARGET is a fully qualified name of a test target within the PyType
+source tree. If no target is specified, all test targets listed in the
 CMake files will be run.
 """
 
@@ -17,41 +17,35 @@ import subprocess
 import sys
 
 import build_utils
-import test_module
 
 def parse_args():
-  """Parse the args to this script and return the list of modules passed."""
+  """Parse the args to this script and return them."""
   parser = argparse.ArgumentParser()
-  parser.add_argument("modules", metavar="MODULE", nargs="*",
-                      help="List of test modules to run.")
+  parser.add_argument("targets", metavar="TARGET", nargs="*",
+                      help="List of test targets to run.")
   parser.add_argument("--fail_fast", "-f", action="store_true", default=False,
                       help="Fail as soon as one build target fails.")
   parser.add_argument("--debug", "-d", action="store_true", default=False,
                       help="Build targets in the debug mode.")
   args = parser.parse_args()
-  for module in args.modules:
-    if "." in module:
-      _, mod_name = module.rsplit(".", 1)
+  for target in args.targets:
+    if "." in target:
+      _, target_name = target.rsplit(".", 1)
     else:
-      mod_name = module
-    if not (mod_name.startswith("test_") or mod_name.endswith("_test")):
-      sys.exit("The name '%s' is not a valid test module name." % module)
-    path = module.replace(".", os.path.sep) + ".py"
-    if not os.path.exists(os.path.join(build_utils.PYTYPE_SRC_ROOT, path)):
-      sys.exit("Module '%s' does not exist.")
+      target_name = target
+    if not (target_name.startswith("test_") or target_name.endswith("_test")):
+      sys.exit("The name '%s' is not a valid test target name." % target)
   return args
 
 
 def main():
   opts = parse_args()
-  modules = opts.modules or ["test_all"]
+  targets = opts.targets or ["test_all"]
   if not build_utils.run_cmake(log_output=True, debug_build=opts.debug):
     sys.exit(1)
   fail_collector = build_utils.FailCollector()
-  # PyType's target names use the dotted name convention. So, the fully
-  # qualified test module names are actually ninja target names.
   print("Running tests (build steps will be executed as required) ...\n")
-  if not build_utils.run_ninja(modules, fail_collector, opts.fail_fast):
+  if not build_utils.run_ninja(targets, fail_collector, opts.fail_fast):
     fail_collector.print_report()
     sys.exit(1)
   print("!!! All tests passed !!!\n"
