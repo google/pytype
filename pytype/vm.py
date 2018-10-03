ff4f3eeca64098dc0fa1a9e27a0b75d2e2d39be7
mdemello <mdemello@google.com>
2018-10-03 13:29:31 -0700

    Do not attempt to load a submodule we haven't explictly imported.
    Fixes #175 [https://github.com/google/pytype/issues/175]
    
    PiperOrigin-RevId: 215616894

--- pytype/vm.py
+++ pytype/vm.py
@@ -143,6 +143,7 @@ class VirtualMachine(object):
     self.director = None
     self._analyzing = False  # Are we in self.analyze()?
     self.opcode_traces = []
+    self._importing = False  # Are we importing another file?
 
     # Map from builtin names to canonical objects.
     self.special_builtins = {
@@ -222,6 +223,7 @@ class VirtualMachine(object):
     """
     _opcode_counter.inc(op.name)
     self.frame.current_opcode = op
+    self._importing = "IMPORT" in op.__class__.__name__
     if log.isEnabledFor(logging.INFO):
       self.log_opcode(op, state)
     try:
@@ -1220,8 +1222,16 @@ class VirtualMachine(object):
     return abstract.LazyConcreteDict(name, d, self)
 
   def import_module(self, name, full_name, level):
+    """Import a module and return the module object or None."""
+    # Do not import new modules if we aren't in an IMPORT statement.
+    # The exception is if we have an implicit "package" module (e.g.
+    # `import a.b.c` adds `a.b` to the list of instantiable modules.)
+    if not (self._importing or self.loader.has_module_prefix(full_name)):
+      return None
     try:
       module = self._import_module(name, level)
+      # Since we have explicitly imported full_name, add it to the prefix list.
+      self.loader.add_module_prefixes(full_name)
     except (parser.ParseError, load_pytd.BadDependencyError,
             visitors.ContainerError, visitors.SymbolLookupError) as e:
       self.errorlog.pyi_error(self.frames, full_name, e)
