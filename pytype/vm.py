2bf48c67c841d9f20ce19360802cec6c7488f9cd
mdemello <mdemello@google.com>
2018-10-05 15:02:48 -0700

    Put strict module checking behind a flag.
    PiperOrigin-RevId: 215972193

--- pytype/vm.py
+++ pytype/vm.py
@@ -1223,11 +1223,12 @@ class VirtualMachine(object):
 
   def import_module(self, name, full_name, level):
     """Import a module and return the module object or None."""
-    # Do not import new modules if we aren't in an IMPORT statement.
-    # The exception is if we have an implicit "package" module (e.g.
-    # `import a.b.c` adds `a.b` to the list of instantiable modules.)
-    if not (self._importing or self.loader.has_module_prefix(full_name)):
-      return None
+    if self.options.strict_import:
+      # Do not import new modules if we aren't in an IMPORT statement.
+      # The exception is if we have an implicit "package" module (e.g.
+      # `import a.b.c` adds `a.b` to the list of instantiable modules.)
+      if not (self._importing or self.loader.has_module_prefix(full_name)):
+        return None
     try:
       module = self._import_module(name, level)
       # Since we have explicitly imported full_name, add it to the prefix list.
