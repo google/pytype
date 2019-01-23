10189059948e8c9a7eecf64c56cce79c6f0f8651
mdemello <mdemello@google.com>
2019-01-22 16:23:55 -0800

    Move the xref arg parser into a library so that other clients can use it.
    PiperOrigin-RevId: 230433629

--- pytype/tools/xref/main.py
+++ pytype/tools/xref/main.py
@@ -4,52 +4,21 @@
 
 from __future__ import print_function
 
-import argparse
 import signal
 import sys
 
-from pytype import config as pytype_config
 from pytype import utils
 from pytype.pytd.parse import node
 
-from pytype.tools import arg_parser
-
 from pytype.tools.xref import debug
 from pytype.tools.xref import indexer
-from pytype.tools.xref import kythe
+from pytype.tools.xref import parse_args
 from pytype.tools.xref import output
 
 
-def make_parser():
-  """Make parser for command line args.
-
-  Returns:
-    A Parser object.
-  """
-
-  def add_kythe_field(parser, field):
-    parser.add_argument(
-        "--" + field, dest=field, type=str, action="store", default="",
-        help="Part of kythe's file-level vname proto.")
-
-  parser = argparse.ArgumentParser(usage="%(prog)s [options] input")
-  add_kythe_field(parser, "kythe_corpus")
-  add_kythe_field(parser, "kythe_root")
-  parser.add_argument("inputs", metavar="input", nargs=1,
-                      help="A .py file to index")
-
-  # Add options from pytype-single.
-  wrapper = arg_parser.ParserWrapper(parser)
-  pytype_config.add_basic_options(wrapper)
-  return arg_parser.Parser(parser, wrapper.actions)
-
-
 def main():
-  parser = make_parser()
   try:
-    args = parser.parse_args(sys.argv[1:])
-    options = pytype_config.Options(args.inputs)
-    options.tweak(**parser.get_pytype_kwargs(args))
+    _, kythe_args, options = parse_args.parse_args(sys.argv[1:])
   except utils.UsageError as e:
     print(str(e), file=sys.stderr)
     sys.exit(1)
@@ -59,7 +28,6 @@ def main():
   if options.timeout is not None:
     signal.alarm(options.timeout)
 
-  kythe_args = kythe.Args(corpus=args.kythe_corpus, root=args.kythe_root)
   ix = indexer.process_file(options, kythe_args=kythe_args)
   if options.debug:
     debug.show_index(ix)
