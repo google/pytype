9e0eb2ef70fac094a2ac53e4438b55b8e8545346
mdemello <mdemello@google.com>
2019-02-19 14:55:34 -0800

    FIX: xref handles --debug itself, not via pytype's options.
    PiperOrigin-RevId: 234680134

--- pytype/tools/xref/main.py
+++ pytype/tools/xref/main.py
@@ -18,7 +18,7 @@ from pytype.tools.xref import output
 
 def main():
   try:
-    _, kythe_args, options = parse_args.parse_args(sys.argv[1:])
+    args, kythe_args, options = parse_args.parse_args(sys.argv[1:])
   except utils.UsageError as e:
     print(str(e), file=sys.stderr)
     sys.exit(1)
@@ -29,7 +29,7 @@ def main():
     signal.alarm(options.timeout)
 
   ix = indexer.process_file(options, kythe_args=kythe_args)
-  if options.debug:
+  if args.debug:
     debug.show_index(ix)
   else:
     output.output_kythe_graph(ix)
