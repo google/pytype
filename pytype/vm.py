d0b3336de375559a5dc69db05e8fe1881657b19d
rechen <rechen@google.com>
2019-03-18 12:58:36 -0700

    Fix the visibility of variable annotations in Python 3.7.
    Fixes the test failure seen here:
    https://travis-ci.org/google/pytype/jobs/507287176.
    
    PiperOrigin-RevId: 239043386

--- pytype/vm.py
+++ pytype/vm.py
@@ -1868,6 +1868,7 @@ class VirtualMachine(object):
   def byte_STORE_SUBSCR(self, state, op):
     """Implement obj[subscr] = val."""
     state, (val, obj, subscr) = state.popn(3)
+    state = state.forward_cfg_node()
     if self._is_annotations_dict(obj):
       try:
         name = abstract_utils.get_atomic_python_constant(
@@ -1876,7 +1877,6 @@ class VirtualMachine(object):
         pass
       else:
         state = self._store_annotation(state, name, val)
-    state = state.forward_cfg_node()
     state = self.store_subscr(state, obj, subscr, val)
     return state
 
