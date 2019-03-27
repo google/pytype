e7e09520a57f1add8224a9dc8b22eed52be17110
rechen <rechen@google.com>
2019-03-27 11:43:58 -0700

    Fix a lint warning I noticed while importing a PR.
    PiperOrigin-RevId: 240608328

--- pytype/tools/analyze_project/config_test.py
+++ pytype/tools/analyze_project/config_test.py
@@ -114,6 +114,7 @@ class TestGenerateConfig(unittest.TestCase):
 
   @classmethod
   def setUpClass(cls):
+    super(TestGenerateConfig, cls).setUpClass()
     cls.parser = parse_args.make_parser()
 
   def test_bad_location(self):
