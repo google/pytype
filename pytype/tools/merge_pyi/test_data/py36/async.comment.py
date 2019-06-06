171f1eb889be355f7f3b1c3f8443ffcdcc6d5715
rechen <rechen@google.com>
2019-06-06 15:13:48 -0700

    Fix a merge-pyi crash in Python versions before 3.6.
    pyi files are always supposed to use the syntax from the latest Python version.
    The lib2to3 grammar file before Python 3.6 can parse most type annotations but
    not PEP 526 variable annotations.
    (Even more confusingly, this problem only shows up in open source tests because
    our internal copy of lib2to3 uses the Python 3 grammar file!)
    
    We work around this issue by patching in a local copy of the grammar with
    support for PEP 526. I tried doing this a bunch of different ways, and the only
    one I found that worked was to apply the patch first thing at import time,
    since merge_pyi goes on to compile a bunch of patterns, etc. This means that we
    can't use a contextmanager, so we leave lib2to3 permanently changed =(
    
    Another issue was that our local grammar doesn't support async and await, since
    they require much more extensive changes to lib2to3. Consequently, we only use
    it in host Python versions before 3.6, so that async and await can still be
    parsed with 3.6+.
    
    I'll follow up with a PR that adds a patch showing the changes I made to the
    grammar and adds the new grammar file to setup.py.
    
    Makes progress toward https://github.com/google/pytype/issues/332.
    
    #pyfixit
    
    PiperOrigin-RevId: 251936937

---
+++ pytype/tools/merge_pyi/test_data/py36/async.comment.py
@@ -0,0 +1,3 @@
+async def f():
+  # type: () -> int
+  pass
---
+++ pytype/tools/merge_pyi/test_data/var_annot.pep484.py
@@ -0,0 +1,3 @@
+# We don't (yet?) merge variable annotations. This test is to make sure we can
+# handle the PEP 526 annotation syntax in pyi files without crashing.
+v = ""
