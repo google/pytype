06e03ec71a7f820b2d0bb8cce60eba2a934a7c76
rechen <rechen@google.com>
2019-08-02 14:24:29 -0700

    Add pytype.tools.traces.traces.trace().
    * Implements the traces.trace() method that generates pytd traces.
    * For now, the only place this method is used is in traces_test, but it'll make
      writing tests for traces.MatchAstVisitor easier.
    
    PiperOrigin-RevId: 261391277

--- pytype/tools/traces/traces.py
+++ pytype/tools/traces/traces.py
@@ -1,7 +1,61 @@
 # Lint as: python2, python3
 """A library for accessing pytype's inferred local types."""
 
+from pytype import analyze
+from pytype import config
+from pytype import errors
+from pytype import load_pytd
+from pytype.pytd import pytd
+from pytype.pytd import pytd_utils
+from pytype.pytd import visitors
+
+from pytype.tools.traces import source
 from pytype.tools.traces import visitor
+
+
+class TypeTrace(source.AbstractTrace):
+  """Traces of inferred type information."""
+
+
+def trace(src, options=None):
+  """Generates type traces for the given source code.
+
+  Args:
+    src: The source text.
+    options: A pytype.config.Options object that can be used to specify options
+      such as the target Python version.
+
+  Returns:
+    A source.Code object.
+  """
+  errorlog = errors.ErrorLog()
+  options = options or config.Options.create()
+  loader = load_pytd.create_loader(options)
+  vm = analyze.CallTracer(
+      errorlog=errorlog,
+      options=options,
+      generate_unknowns=options.protocols,
+      loader=loader)
+  pytd_module, _ = analyze.infer_types(
+      src=src,
+      filename=options.input,
+      errorlog=errorlog,
+      options=options,
+      loader=loader,
+      tracer_vm=vm)
+  raw_traces = []
+  for op, symbol, data in vm.opcode_traces:
+    raw_traces.append(
+        (op, symbol, tuple(_to_pytd(d, loader, pytd_module) for d in data)))
+  return source.Code(src, raw_traces, TypeTrace, options.input)
+
+
+def _to_pytd(datum, loader, ast):
+  if not datum:
+    return pytd.AnythingType()
+  t = pytd_utils.JoinTypes(v.to_type() for v in datum).Visit(
+      visitors.RemoveUnknownClasses())
+  return loader.resolve_type(t, ast)
 
 
 class MatchAstVisitor(visitor.BaseVisitor):
--- pytype/tools/traces/traces_test.py
+++ pytype/tools/traces/traces_test.py
@@ -2,13 +2,9 @@
 """Tests for traces.traces."""
 
 import ast
-from pytype.tools.traces import source
+from pytype import config
 from pytype.tools.traces import traces
 import unittest
-
-
-class _FakeTrace(source.AbstractTrace):
-  """Fake trace class for testing."""
 
 
 class _NotImplementedVisitor(traces.MatchAstVisitor):
@@ -17,12 +13,41 @@ class _NotImplementedVisitor(traces.MatchAstVisitor):
     self.match(node)
 
 
+class TraceTest(unittest.TestCase):
+  """Tests for traces.trace."""
+
+  def test_traces(self):
+    src = traces.trace("")
+    trace, = src.traces[1]
+    self.assertEqual(trace.op, "LOAD_CONST")
+    self.assertEqual(trace.symbol, None)
+    pyval, = trace.types
+    self.assertEqual(pyval.name, "__builtin__.NoneType")
+    self.assertEqual(pyval.cls.name, "__builtin__.NoneType")
+
+  def test_options(self):
+    src = traces.trace("", config.Options.create("rumpelstiltskin"))
+    self.assertEqual(src.filename, "rumpelstiltskin")
+
+  def test_external_type(self):
+    pyi = self.create_tempfile(
+        file_path="foo.pyi", content="class Foo(object): ...")
+    imports_info = self.create_tempfile(content="foo %s" % pyi.full_path)
+    src = traces.trace(
+        "import foo\nx = foo.Foo()",
+        config.Options.create(imports_map=imports_info.full_path))
+    trace, = (x for x in src.traces[2] if x.op == "STORE_NAME")
+    pyval, = trace.types
+    self.assertEqual(pyval.name, "foo.Foo")
+    self.assertEqual(pyval.cls.name, "foo.Foo")
+
+
 class MatchAstVisitorTest(unittest.TestCase):
   """Tests for traces.MatchAstVisitor."""
 
   def test_not_implemented(self):
     module = ast.parse("")
-    src = source.Code("", [], _FakeTrace, "")
+    src = traces.trace("")
     v = _NotImplementedVisitor(src, ast)
     with self.assertRaises(NotImplementedError):
       v.visit(module)
