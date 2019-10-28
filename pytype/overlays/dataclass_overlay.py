957bc69ace836632f98be635fc30bc03ac6d8d80
rechen <rechen@google.com>
2019-10-28 14:51:30 -0700

    Temporarily make dataclass error checking more lenient on None.
    PiperOrigin-RevId: 277148225

--- pytype/overlays/dataclass_overlay.py
+++ pytype/overlays/dataclass_overlay.py
@@ -70,8 +70,10 @@ class Dataclass(classgen.Decorator):
         bad = self.vm.matcher.bad_matches(orig, typ, node)
         if bad:
           binding = bad[0][orig]
-          self.vm.errorlog.annotation_type_mismatch(
-              self.vm.frames, typ, binding, name)
+          # TODO(b/143487719): re-enable checking on None.
+          if binding.data != self.vm.convert.none:
+            self.vm.errorlog.annotation_type_mismatch(
+                self.vm.frames, typ, binding, name)
 
       attr = classgen.Attribute(name=name, typ=value, init=init, default=orig)
       own_attrs.append(attr)
