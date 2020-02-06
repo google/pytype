834d97560c08f0134d9221c1a09b3024ac6ec5ae
Paul Ganssle <paul@ganssle.io>
2020-02-06 09:57:28 -0500

    Add Python 3.7 to trove classifiers

--- setup.py
+++ setup.py
@@ -167,6 +167,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Software Development',
     ],
