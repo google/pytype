0a88c08b508552c9a50f654c3475b35893f36a27
Paul Ganssle <paul@ganssle.io>
2020-02-10 11:58:18 -0500

    Add Python 3.5 to trove classifiers

--- setup.py
+++ setup.py
@@ -167,6 +167,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
