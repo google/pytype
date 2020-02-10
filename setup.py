d896984a4e4a3d8b88fd274142eaf4d83ac45ccb
Paul Ganssle <paul@ganssle.io>
2020-02-10 16:52:54 -0500

    Further modernize python build system (#509)
    * Replace conditional logic with environment markers
    
    PEP 496 introduced environment markers, which allow you to declare a
    dependency conditional on the environment the package is to be installed
    into. Among other things, this is useful for cross-compilation.
    
    * Add Python 3.7 to trove classifiers
    
    * Add python_requires matching test matrix
    
    This will prevent `pip` from finding versions of pytype that do not
    support a given version of Python. Unfortunately, when no
    python_requires declaration is made, the default is "acceptable for all
    versions", and so without yanking old versions, `pip install pytype` on
    unsupported versions will simply fall back to the latest release without
    a `python_requires` declaration. Alas, this still seems like a win.
    
    * Add Python 3.5 to trove classifiers
    
    * Move all static build options into setup.cfg
    
    Using the declarative build file for all your declarative options is
    current best practice, since it makes it much easier to parse metadata
    from projects without executing arbitrary code.
    
    This will change the minimum required version of setuptools to 30.4,
    which was released in December 2016.
    
    * Add pyproject.toml file with build-system
    
    This declares the build system and (Python) dependencies required to
    build the project, and will trigger PEP 517 builds when present in pip
    installs.
    
    * Add pyproject.toml to MANIFEST.in
    
    This will include pyproject.toml in sdists, which will trigger PEP 517
    builds when installing from sdist.
    
    * Add license to build metadata
    
    * Use find_packages to populate the package list
    
    This will prevent further issues like #372 (though technically #372
    would still have caused a problem because __init__.py was missing).
    
    * Add project_urls
    
    This allows PyPI to give multiple links to various aspects of the
    project. See an example of how this renders on urllib3's PyPI page:
    https://pypi.org/project/urllib3/1.25.8/
    
    * Move version into setup.cfg
    
    Importing from a fixed module is a built-in capability of the
    declarative build file, so no need to re-invent it ourselves.

--- setup.py
+++ setup.py
@@ -12,16 +12,19 @@ import sys
 
 from setuptools import setup, Extension  # pylint: disable=g-multiple-import
 
+# Path to directory containing setup.py
+here = os.path.abspath(os.path.dirname(__file__))
+
 try:
+  # The repository root is not on the pythonpath with PEP 517 builds
+  if 'build_scripts' in os.listdir(here):
+      sys.path.append(here)
+
   from build_scripts import build_utils  # pylint: disable=g-import-not-at-top
 except ImportError:
   # When build_utils is present, we'll generate parser files for installing
   # from source or packaging into a PyPI release.
   build_utils = None
-
-
-# Path to directory containing setup.py
-here = os.path.abspath(os.path.dirname(__file__))
 
 
 def get_parser_ext():
@@ -105,78 +108,18 @@ def get_long_description():
       r'[\g<1>]: https://github.com/google/pytype/blob/master/docs/', desc)
 
 
-def get_version():
-  # Load the package's __version__.py module as a dictionary.
-  about = {}
-  with open(os.path.join(here, 'pytype', '__version__.py')) as f:
-    exec(f.read(), about)  # pylint: disable=exec-used
-  return about['__version__']
-
-
-def get_install_requires():
-  requires = [
-      'attrs',
-      'importlab (>=0.5.1)',
-      'ninja',
-      'pyyaml (>=3.11)',
-      'six',
-  ]
-  if sys.version_info >= (3, 3):
-    requires.append('typed_ast')
-  return requires
-
-
 copy_typeshed()
 if build_utils:
   e = build_utils.generate_files()
   assert not e, e
+
+# Only options configured at build time are declared here, everything else is
+# declared in setup.cfg
 setup(
-    name='pytype',
-    version=get_version(),
-    description='Python type inferencer',
     long_description=get_long_description(),
-    long_description_content_type='text/markdown',
-    maintainer='Google',
-    maintainer_email='pytype@googlegroups.com',
-    url='https://google.github.io/pytype',
-    packages=[
-        'pytype',
-        'pytype/overlays',
-        'pytype/pyc',
-        'pytype/pyi',
-        'pytype/pytd',
-        'pytype/pytd/parse',
-        'pytype/tools',
-        'pytype/tools/analyze_project',
-        'pytype/tools/annotate_ast',
-        'pytype/tools/merge_pyi',
-        'pytype/tools/traces',
-        'pytype/tools/xref',
-        'pytype/typegraph',
-    ],
-    entry_points={
-        'console_scripts': [
-            'annotate-ast = pytype.tools.annotate_ast.main:main',
-            'merge-pyi = pytype.tools.merge_pyi.main:main',
-            'pytd = pytype.pytd.main:main',
-            'pytype = pytype.tools.analyze_project.main:main',
-            'pytype-single = pytype.single:main',
-            'pyxref = pytype.tools.xref.main:main',
-        ]
-    },
     package_data={'pytype': get_data_files()},
-    install_requires=get_install_requires(),
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Software Development',
-    ],
     ext_modules=[get_parser_ext()],
 )
+
 if build_utils:
   build_utils.clean_generated_files()
