bf33b1ae4b29e5b6bd2d3e4a1b599380b3c0c020
tsudol <tsudol@google.com>
2020-05-19 14:37:48 -0700

    Treat NoReturn as a singleton.
    This was preventing several checks in vm.py from returning true due to
    comparing NoReturn to Instance<NoReturn> when NoReturn was returned from a
    function call.
    
    #pyfixit
    
    PiperOrigin-RevId: 312355551

--- pytype/overlays/typing_overlay.py
+++ pytype/overlays/typing_overlay.py
@@ -211,6 +211,7 @@ class Cast(abstract.PyTDFunction):
 
 
 class NoReturn(abstract.AtomicAbstractValue):
+  """Implements typing.NoReturn as a singleton."""
 
   def __init__(self, vm):
     super(NoReturn, self).__init__("NoReturn", vm)
@@ -220,6 +221,15 @@ class NoReturn(abstract.AtomicAbstractValue):
 
   def compute_mro(self):
     return self.default_mro()
+
+  def call(self, node, func, args, alias_map=None):
+    del func, args
+    # Since this is a singleton, return ourself. (e.g. abstract.Unsolvable)
+    return node, self.to_variable(node)
+
+  def instantiate(self, node, container=None):
+    # Return ourself.
+    return self.to_variable(node)
 
 
 def build_any(name, vm):
