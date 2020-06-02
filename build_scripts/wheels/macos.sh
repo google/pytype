0f93b66f81e0d89944f81d3710fdcd4e6d0ced42
Brad Solomon <bsolomon@protonmail.com>
2020-06-02 09:45:07 -0400

    Remove pyenv refs

--- build_scripts/wheels/macos.sh
+++ build_scripts/wheels/macos.sh
@@ -3,17 +3,11 @@
 set -eux
 
 brew update
-brew upgrade pyenv
-eval "$(pyenv init -)"
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-pyenv --version
-pyenv install -s "$PYENV_VERSION"
-pyenv local "$PYENV_VERSION"
 python -V
 brew install -v bison || brew upgrade bison
 brew install -v cmake || brew upgrade cmake
 brew install -v ninja || brew upgrade ninja
 python -m pip install -U pip setuptools wheel
-export CMAKE_PREFIX_PATH="$(brew --prefix bison)"
+CMAKE_PREFIX_PATH="$(brew --prefix bison)"
+export CMAKE_PREFIX_PATH
 python -m pip wheel . --verbose --no-deps -w dist
