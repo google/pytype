545ae5c1691e93b819192b37f671542e679f9239
rechen <rechen@google.com>
2020-07-09 18:42:59 -0700

    Fix a spurious error caused by calling get_class() on a TypeParameterInstance.
    Without this fix, pytype reports an invalid-typevar when trying to call
    Foo.foo() because calling .items() on an OrderedDict[str, Foo] produces
    (TypeParameterInstance(_K), TypeParameterInstance(_V)) tuples, so the first
    argument to foo() ends up being TypeParameterInstance(_V).get_class(). Instead,
    we should resolve the TypeParameterInstance immediately.
    
    PiperOrigin-RevId: 320523343

--- pytype/abstract.py
+++ pytype/abstract.py
@@ -1734,10 +1734,14 @@ class PyTDFunction(Function):
       return StaticMethod(self.name, self, callself, self.vm)
     elif self.kind == pytd.CLASSMETHOD:
       if not is_class:
-        # callself is the instance, and we want to bind to its class.
         callself = abstract_utils.get_atomic_value(
-            callself, default=self.vm.convert.unsolvable
-        ).get_class().to_variable(self.vm.root_cfg_node)
+            callself, default=self.vm.convert.unsolvable)
+        if isinstance(callself, TypeParameterInstance):
+          callself = abstract_utils.get_atomic_value(
+              callself.instance.get_instance_type_parameter(callself.name),
+              default=self.vm.convert.unsolvable)
+        # callself is the instance, and we want to bind to its class.
+        callself = callself.get_class().to_variable(self.vm.root_cfg_node)
       return ClassMethod(self.name, self, callself, self.vm)
     elif self.kind == pytd.PROPERTY and not is_class:
       return Property(self.name, self, callself, self.vm)
