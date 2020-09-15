bc7154887f379825757de127ec398fba8a83876d
rechen <rechen@google.com>
2020-09-15 13:08:55 -0700

    Fix vm_test failure in 3.8.
    Fixes the most egregious failure in vm_test, which is that
    dis.opmap["SETUP_LOOP"] does not exist.
    
    For https://github.com/google/pytype/issues/440.
    
    PiperOrigin-RevId: 331837381

--- pytype/vm_test.py
+++ pytype/vm_test.py
@@ -1,6 +1,5 @@
 """Tests for vm.py."""
 
-import dis
 import textwrap
 
 from pytype import analyze
@@ -9,11 +8,14 @@ from pytype import compat
 from pytype import errors
 from pytype import utils
 from pytype import vm
+from pytype.pyc import opcodes
 from pytype.pyc import pyc
 from pytype.tests import test_base
 from pytype.tests import test_utils
 
 import six
+
+_PY2_OPMAP = {v.__name__: k for k, v in opcodes.python2_mapping.items()}
 
 
 class TraceVM(vm.VirtualMachine):
@@ -99,38 +101,38 @@ class BytecodeTest(test_base.BaseTest, test_utils.MakeCodeMixin):
           z += x*a
     """)
   code_nested_loop = compat.int_array_to_bytes([
-      dis.opmap["LOAD_CONST"], 0, 0,          # [0], 0, arg=0
-      dis.opmap["LOAD_CONST"], 1, 0,          # [1], 3, arg=1
-      dis.opmap["LOAD_CONST"], 2, 0,          # [2], 6, arg=2
-      dis.opmap["BUILD_LIST"], 3, 0,          # [3], 9, arg=3
-      dis.opmap["STORE_NAME"], 0, 0,          # [4], 12, arg=0
-      dis.opmap["LOAD_CONST"], 3, 0,          # [5], 15, arg=3
-      dis.opmap["STORE_NAME"], 1, 0,          # [6], 18, arg=1
-      dis.opmap["SETUP_LOOP"], 54, 0,         # [7], 21, dest=78
-      dis.opmap["LOAD_NAME"], 0, 0,           # [8], 24, arg=0
-      dis.opmap["GET_ITER"],                  # [9], 27
-      dis.opmap["FOR_ITER"], 46, 0,           # [10], 28, dest=77
-      dis.opmap["STORE_NAME"], 2, 0,          # [11], 31, arg=2
-      dis.opmap["SETUP_LOOP"], 37, 0,         # [12], 34, dest=74
-      dis.opmap["LOAD_NAME"], 0, 0,           # [13], 37, arg=0
-      dis.opmap["GET_ITER"],                  # [14], 40
-      dis.opmap["FOR_ITER"], 29, 0,           # [15], 41, dest=73
-      dis.opmap["STORE_NAME"], 3, 0,          # [16], 44, arg=3
-      dis.opmap["LOAD_NAME"], 2, 0,           # [17], 47, arg=2
-      dis.opmap["POP_JUMP_IF_FALSE"], 41, 0,  # [18], 50, dest=41
-      dis.opmap["LOAD_NAME"], 1, 0,           # [19], 53, arg=1
-      dis.opmap["LOAD_NAME"], 2, 0,           # [20], 56, arg=2
-      dis.opmap["LOAD_NAME"], 3, 0,           # [21], 59, arg=3
-      dis.opmap["BINARY_MULTIPLY"],           # [22], 62
-      dis.opmap["INPLACE_ADD"],               # [23], 63
-      dis.opmap["STORE_NAME"], 1, 0,          # [24], 64, arg=1
-      dis.opmap["JUMP_ABSOLUTE"], 41, 0,      # [25], 67, dest=41
-      dis.opmap["JUMP_ABSOLUTE"], 41, 0,      # [26], 70 (unreachable), dest=41
-      dis.opmap["POP_BLOCK"],                 # [27], 73
-      dis.opmap["JUMP_ABSOLUTE"], 28, 0,      # [28], 74, dest=28
-      dis.opmap["POP_BLOCK"],                 # [29], 77
-      dis.opmap["LOAD_CONST"], 4, 0,          # [30], 78, arg=4
-      dis.opmap["RETURN_VALUE"],              # [31], 81
+      _PY2_OPMAP["LOAD_CONST"], 0, 0,          # [0], 0, arg=0
+      _PY2_OPMAP["LOAD_CONST"], 1, 0,          # [1], 3, arg=1
+      _PY2_OPMAP["LOAD_CONST"], 2, 0,          # [2], 6, arg=2
+      _PY2_OPMAP["BUILD_LIST"], 3, 0,          # [3], 9, arg=3
+      _PY2_OPMAP["STORE_NAME"], 0, 0,          # [4], 12, arg=0
+      _PY2_OPMAP["LOAD_CONST"], 3, 0,          # [5], 15, arg=3
+      _PY2_OPMAP["STORE_NAME"], 1, 0,          # [6], 18, arg=1
+      _PY2_OPMAP["SETUP_LOOP"], 54, 0,         # [7], 21, dest=78
+      _PY2_OPMAP["LOAD_NAME"], 0, 0,           # [8], 24, arg=0
+      _PY2_OPMAP["GET_ITER"],                  # [9], 27
+      _PY2_OPMAP["FOR_ITER"], 46, 0,           # [10], 28, dest=77
+      _PY2_OPMAP["STORE_NAME"], 2, 0,          # [11], 31, arg=2
+      _PY2_OPMAP["SETUP_LOOP"], 37, 0,         # [12], 34, dest=74
+      _PY2_OPMAP["LOAD_NAME"], 0, 0,           # [13], 37, arg=0
+      _PY2_OPMAP["GET_ITER"],                  # [14], 40
+      _PY2_OPMAP["FOR_ITER"], 29, 0,           # [15], 41, dest=73
+      _PY2_OPMAP["STORE_NAME"], 3, 0,          # [16], 44, arg=3
+      _PY2_OPMAP["LOAD_NAME"], 2, 0,           # [17], 47, arg=2
+      _PY2_OPMAP["POP_JUMP_IF_FALSE"], 41, 0,  # [18], 50, dest=41
+      _PY2_OPMAP["LOAD_NAME"], 1, 0,           # [19], 53, arg=1
+      _PY2_OPMAP["LOAD_NAME"], 2, 0,           # [20], 56, arg=2
+      _PY2_OPMAP["LOAD_NAME"], 3, 0,           # [21], 59, arg=3
+      _PY2_OPMAP["BINARY_MULTIPLY"],           # [22], 62
+      _PY2_OPMAP["INPLACE_ADD"],               # [23], 63
+      _PY2_OPMAP["STORE_NAME"], 1, 0,          # [24], 64, arg=1
+      _PY2_OPMAP["JUMP_ABSOLUTE"], 41, 0,      # [25], 67, dest=41
+      _PY2_OPMAP["JUMP_ABSOLUTE"], 41, 0,      # [26], 70 (unreachable), dest=41
+      _PY2_OPMAP["POP_BLOCK"],                 # [27], 73
+      _PY2_OPMAP["JUMP_ABSOLUTE"], 28, 0,      # [28], 74, dest=28
+      _PY2_OPMAP["POP_BLOCK"],                 # [29], 77
+      _PY2_OPMAP["LOAD_CONST"], 4, 0,          # [30], 78, arg=4
+      _PY2_OPMAP["RETURN_VALUE"],              # [31], 81
   ])
 
   def test_each_instruction_once_loops(self):
@@ -153,17 +155,17 @@ class BytecodeTest(test_base.BaseTest, test_utils.MakeCodeMixin):
     x = 42
     """)
   code_deadcode = compat.int_array_to_bytes([
-      dis.opmap["LOAD_NAME"], 0, 0,           # [0] 0, arg=0
-      dis.opmap["POP_JUMP_IF_FALSE"], 15, 0,  # [1] 3, dest=15
-      dis.opmap["LOAD_CONST"], 0, 0,          # [2] 6, arg=0
-      dis.opmap["STORE_NAME"], 1, 0,          # [3] 9, arg=1
-      dis.opmap["JUMP_FORWARD"], 0, 0,        # [4] 12, dest=15
-      dis.opmap["LOAD_NAME"], 2, 0,           # [5] 15, arg=2
-      dis.opmap["RAISE_VARARGS"], 1, 0,       # [6] 18, arg=1
-      dis.opmap["LOAD_CONST"], 1, 0,          # [7] 21 (unreachable), arg=1
-      dis.opmap["STORE_NAME"], 1, 0,          # [8] 24 (unreachable), arg=1
-      dis.opmap["LOAD_CONST"], 2, 0,          # [9] 27 (unreachable), arg=2
-      dis.opmap["RETURN_VALUE"],              # [10] 30 (unreachable)
+      _PY2_OPMAP["LOAD_NAME"], 0, 0,           # [0] 0, arg=0
+      _PY2_OPMAP["POP_JUMP_IF_FALSE"], 15, 0,  # [1] 3, dest=15
+      _PY2_OPMAP["LOAD_CONST"], 0, 0,          # [2] 6, arg=0
+      _PY2_OPMAP["STORE_NAME"], 1, 0,          # [3] 9, arg=1
+      _PY2_OPMAP["JUMP_FORWARD"], 0, 0,        # [4] 12, dest=15
+      _PY2_OPMAP["LOAD_NAME"], 2, 0,           # [5] 15, arg=2
+      _PY2_OPMAP["RAISE_VARARGS"], 1, 0,       # [6] 18, arg=1
+      _PY2_OPMAP["LOAD_CONST"], 1, 0,          # [7] 21 (unreachable), arg=1
+      _PY2_OPMAP["STORE_NAME"], 1, 0,          # [8] 24 (unreachable), arg=1
+      _PY2_OPMAP["LOAD_CONST"], 2, 0,          # [9] 27 (unreachable), arg=2
+      _PY2_OPMAP["RETURN_VALUE"],              # [10] 30 (unreachable)
   ])
 
   def test_each_instruction_once_dead_code(self):
