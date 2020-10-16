6ab9a31ce770295c1ce258bc8356f1c3925c0b0e
Shane Harvey <shnhrv@gmail.com>
2020-10-15 22:40:48 -0700

    Fix tests for comment and pep484

--- pytype/tools/merge_pyi/test_data/imports_alias.comment.py
+++ pytype/tools/merge_pyi/test_data/imports_alias.comment.py
@@ -3,7 +3,9 @@ from m1 import A_old as A
 from m2 import B_old as B
 from m3 import C_old as C
 import m4_old as m4
+import m5.D_old as D
+import m5.something.E_old as E
 
-def f(a, b, c):
-    # type: (A, B, C) -> m4.D
+def f(a, b, c, d, e):
+    # type: (A, B, C, D, E) -> m4.D
     pass
--- pytype/tools/merge_pyi/test_data/imports_alias.pep484.py
+++ pytype/tools/merge_pyi/test_data/imports_alias.pep484.py
@@ -3,6 +3,8 @@ from m1 import A_old as A
 from m2 import B_old as B
 from m3 import C_old as C
 import m4_old as m4
+import m5.D_old as D
+import m5.something.E_old as E
 
-def f(a: A, b: B, c: C) -> m4.D:
+def f(a: A, b: B, c: C, d: D, e: E) -> m4.D:
     pass
