28af06f62e67ae840e5db516288df4d9eef73450
rechen <rechen@google.com>
2021-04-15 13:32:46 -0700

    Add some more support for generic type aliases.
    With this change, generic type aliases should be working for most container
    types (Dict, List, Tuple, etc.). They still don't work with a few special
    containers in typing_overlay (Callable, Literal, Union); I've added skipped
    tests for those.
    
    TESTED=Rebuilt the pytype pars and ran a TGP:
    []/OCL:368552652:BASE:368554562:1618479527787:c523ed86
    PiperOrigin-RevId: 368707443

--- pytype/abstract.py
+++ pytype/abstract.py
@@ -1383,7 +1383,10 @@ class AnnotationContainer(AnnotationClass):
     Returns:
       A tuple of the template, the parameters, and the container class.
     """
-    template = tuple(t.name for t in self.base_cls.template)
+    if isinstance(self.base_cls, TupleClass):
+      template = tuple(range(self.base_cls.tuple_length))
+    else:
+      template = tuple(t.name for t in self.base_cls.template)
     self.vm.errorlog.invalid_ellipses(
         self.vm.frames, ellipses - allowed_ellipses, self.name)
     last_index = len(inner) - 1
@@ -1391,7 +1394,35 @@ class AnnotationContainer(AnnotationClass):
       # Even if an ellipsis is not allowed at this position, strip it off so
       # that we report only one error for something like 'List[int, ...]'
       inner = inner[:-1]
-    return template, inner, ParameterizedClass
+    if (isinstance(self.base_cls, ParameterizedClass) and
+        # TODO(b/140251808): Support CallableClass.
+        not isinstance(self.base_cls, CallableClass)):
+      # We're dealing with a generic type alias, e.g.:
+      #   X = Dict[T, str]
+      #   def f(x: X[int]): ...
+      # We construct `inner` using both the new inner values and the ones
+      # already in X, to end up with a final result of:
+      #   template=(_K, _V)
+      #   inner=(int, str)
+      new_inner = []
+      inner_idx = 0
+      for k in template:
+        v = self.base_cls.formal_type_parameters[k]
+        if v.formal:
+          new_inner.append(inner[inner_idx])
+          inner_idx += 1
+        else:
+          new_inner.append(v)
+      # Tack on any leftover values so that an error will be reported if `inner`
+      # is the wrong length.
+      inner = tuple(new_inner) + inner[inner_idx:]
+      if isinstance(self.base_cls, TupleClass):
+        template += (abstract_utils.T,)
+        inner += (self.vm.merge_values(inner),)
+      abstract_class = type(self.base_cls)
+    else:
+      abstract_class = ParameterizedClass
+    return template, inner, abstract_class
 
   def _build_value(self, node, raw_inner, ellipses):
     if self.base_cls.is_late_annotation():
@@ -1412,24 +1443,34 @@ class AnnotationContainer(AnnotationClass):
       self.vm.late_annotations[self.base_cls.expr].append(annot)
       return annot
     template, inner, abstract_class = self._get_value_info(raw_inner, ellipses)
-    if self.base_cls.full_name == "typing.Generic":
+    if isinstance(self.base_cls, ParameterizedClass):
+      base_cls = self.base_cls.base_cls
+    else:
+      base_cls = self.base_cls
+    if base_cls.full_name == "typing.Generic":
       # Generic is unique in that parameterizing it defines a new template;
       # usually, the parameterized class inherits the base class's template.
       template_params = [
-          param.with_module(self.base_cls.full_name) for param in inner]
+          param.with_module(base_cls.full_name) for param in inner]
     else:
       template_params = None
     if len(inner) != len(template):
       if not template:
-        self.vm.errorlog.not_indexable(self.vm.frames, self.base_cls.name,
+        self.vm.errorlog.not_indexable(self.vm.frames, base_cls.name,
                                        generic_warning=True)
       else:
         # Use the unprocessed values of `template` and `inner` so that the error
         # message matches what the user sees.
         name = "%s[%s]" % (
-            self.full_name, ", ".join(t.name for t in self.base_cls.template))
+            self.full_name, ", ".join(t.name for t in base_cls.template))
+        if isinstance(self.base_cls, ParameterizedClass):
+          template_length = sum(
+              self.base_cls.formal_type_parameters[k.name].formal
+              for k in base_cls.template)
+        else:
+          template_length = len(base_cls.template)
         error = "Expected %d parameter(s), got %d" % (
-            len(self.base_cls.template), len(raw_inner))
+            template_length, len(raw_inner))
         self.vm.errorlog.invalid_annotation(self.vm.frames, None, error, name)
     else:
       if len(inner) == 1:
@@ -1447,8 +1488,8 @@ class AnnotationContainer(AnnotationClass):
 
     # For user-defined generic types, check if its type parameter matches
     # its corresponding concrete type
-    if isinstance(self.base_cls, InterpreterClass) and self.base_cls.template:
-      for formal in self.base_cls.template:
+    if isinstance(base_cls, InterpreterClass) and base_cls.template:
+      for formal in base_cls.template:
         if (isinstance(formal, TypeParameter) and not formal.is_generic() and
             isinstance(params[formal.name], TypeParameter)):
           if formal.name != params[formal.name].name:
@@ -1467,7 +1508,7 @@ class AnnotationContainer(AnnotationClass):
             return self.vm.convert.unsolvable
 
     try:
-      return abstract_class(self.base_cls, params, self.vm, template_params)
+      return abstract_class(base_cls, params, self.vm, template_params)
     except abstract_utils.GenericTypeError as e:
       self.vm.errorlog.invalid_annotation(self.vm.frames, e.annot, e.error)
       return self.vm.convert.unsolvable
@@ -2194,7 +2235,7 @@ class ParameterizedClass(BaseValue, class_mixin.Class, mixin.NestedAnnotation):
   def __init__(self, base_cls, formal_type_parameters, vm, template=None):
     # A ParameterizedClass is created by converting a pytd.GenericType, whose
     # base type is restricted to NamedType and ClassType.
-    assert isinstance(base_cls, class_mixin.Class)
+    assert isinstance(base_cls, (PyTDClass, InterpreterClass))
     self.base_cls = base_cls
     super().__init__(base_cls.name, vm)
     self.module = base_cls.module
