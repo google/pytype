6292fa44ca2b4430c00fac1328b16b876442a443
Sebastian Rittau <srittau@rittau.biz>
2021-05-03 22:18:36 +0200

    Ignore comments and empty lines in VERSIONS file

--- pytype/pytd/typeshed.py
+++ pytype/pytd/typeshed.py
@@ -117,7 +117,10 @@ class Typeshed:
     _, text = self._load_file(os.path.join("stdlib", "VERSIONS"))
     versions = {}
     for line in text.splitlines():
-      match = re.fullmatch(r"(.+): (\d)\.(\d+)(?:-(?:(\d)\.(\d))?)?", line)
+      line2 = line.split("#")[0].strip()
+      if not line2:
+        continue
+      match = re.fullmatch(r"(.+): (\d)\.(\d+)(?:-(?:(\d)\.(\d))?)?", line2)
       assert match
       module, min_major, min_minor, max_major, max_minor = match.groups()
       minimum = (int(min_major), int(min_minor))
