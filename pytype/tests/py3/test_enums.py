7e21b623bceae696110db2701a4011ddf174ac0f
tsudol <tsudol@google.com>
2021-06-15 15:41:17 -0700

    Add support for enum __getitem__.
    The tests pass with changes to enum.pytd.
    
    PiperOrigin-RevId: 379590020

--- pytype/tests/py3/test_enums.py
+++ pytype/tests/py3/test_enums.py
@@ -86,5 +86,82 @@ class EnumOverlayTest(test_base.TargetPython3FeatureTest):
         v: int
       """)
 
+  @test_base.skip("Fails due to __getattr__ in pytd.")
+  def test_name_lookup(self):
+    with file_utils.Tempdir() as d:
+      d.create_file("e.pyi", "a_string: str")
+      self.CheckWithErrors("""
+        import enum
+        import e
+        class M(enum.Enum):
+          A = 1
+          B = "b"
+        assert_type(M["A"].value, "int")
+        assert_type(M["B"].value, "str")
+        assert_type(M[e.a_string].value, "Any")
+        _ = M["C"]  # attribute-error
+      """, pythonpath=[d.path])
+
+  @test_base.skip("Fails due to __getattr__ in pytd.")
+  def test_name_lookup_pytd(self):
+    with file_utils.Tempdir() as d:
+      d.create_file("e.pyi", """
+        enum: module
+        a_string: str
+        class M(enum.Enum):
+          A: int
+          B: str
+      """)
+      self.CheckWithErrors("""
+        import e
+        assert_type(e.M["A"].value, "int")
+        assert_type(e.M["B"].value, "str")
+        # Canonical PyTD enums are missing name/value fields.
+        # assert_type(e.M[e.a_string].value, "Any")
+        _ = e.M["C"]  # attribute-error
+      """, pythonpath=[d.path])
+
+  def test_bad_name_lookup(self):
+    self.CheckWithErrors("""
+      import enum
+      class M(enum.Enum):
+        A = 1
+      M[1]  # unsupported-operands
+    """)
+
+  @test_base.skip("Fails due to __getattr__ in pytd.")
+  def test_enum_named_name(self):
+    self.Check("""
+      import enum
+      class M(enum.Enum):
+        name = 1
+        value = "hello"
+      assert_type(M.name, "M")
+      assert_type(M.name.name, "str")
+      assert_type(M.name.value, "int")
+      assert_type(M.value, "M")
+      assert_type(M.value.name, "str")
+      assert_type(M.value.value, "str")
+    """)
+
+  @test_base.skip("Fails due to __getattr__ in pytd.")
+  def test_enum_pytd_named_name(self):
+    with file_utils.Tempdir() as d:
+      d.create_file("m.pyi", """
+        enum: module
+        class M(enum.Enum):
+          name: int
+          value: str
+      """)
+      self.Check("""
+        from m import M
+        assert_type(M.name, "m.M")
+        assert_type(M.name.name, "str")
+        assert_type(M.name.value, "int")
+        assert_type(M.value, "m.M")
+        assert_type(M.value.name, "str")
+        assert_type(M.value.value, "str")
+      """, pythonpath=[d.path])
+
 
 test_base.main(globals(), __name__ == "__main__")
