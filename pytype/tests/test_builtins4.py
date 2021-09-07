561fb0546ec673a467d441bc370281f40e1f0822
Gerard <jerry.om.95@gmail.com>
2021-09-07 03:24:16 -0700

    Add `breakpoint` to the supported builtin methods (#997)
    Resolves #994
    
    Resolves #997
    
    PiperOrigin-RevId: 395210499

--- pytype/tests/test_builtins4.py
+++ pytype/tests/test_builtins4.py
@@ -663,6 +663,21 @@ class BuiltinPython3FeatureTest(test_base.BaseTest):
       f(bytearray())
     """)
 
+  @test_utils.skipBeforePy((3, 7), "breakpoint support is new in 3.7")
+  def test_breakpoint(self):
+    self.Check("""
+      breakpoint()
+    """)
+
+  @test_utils.skipFromPy(
+      (3, 7),
+      "breakpoint not available as builtin method until 3.7",
+  )
+  def test_breakpoint_not_defined(self):
+    self.CheckWithErrors("""
+      breakpoint()  # name-error
+    """)
+
 
 if __name__ == "__main__":
   test_base.main()
