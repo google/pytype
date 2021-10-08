049bd44e564563709752dc9cedea0496267745d3
rechen <rechen@google.com>
2021-10-08 12:54:46 -0700

    Fix a crash in WrongArgTypes.__gt__.
    I assumed that the 'other' parameter would also be a WrongArgTypes after a
    certain point without actually doing the proper checks ^^;
    
    PiperOrigin-RevId: 401835902

--- pytype/function.py
+++ pytype/function.py
@@ -619,9 +619,12 @@ class WrongArgTypes(InvalidParameters):
   """For functions that were called with the wrong types."""
 
   def __gt__(self, other):
-    if other is None or (isinstance(other, FailedFunctionCall) and
-                         not isinstance(other, WrongArgTypes)):
+    if other is None:
       return True
+    if not isinstance(other, WrongArgTypes):
+      # WrongArgTypes should take precedence over other FailedFunctionCall
+      # subclasses but not over unrelated errors like DictKeyMissing.
+      return isinstance(other, FailedFunctionCall)
     # The signature that has fewer *args/**kwargs tends to be more precise.
     def starcount(err):
       return (bool(err.bad_call.sig.varargs_name) +
