66ba8ec8452f8d882394d5e72ee9d6204700d4a8
rechen <rechen@google.com>
2021-11-16 20:22:21 -0800

    Allow unknown node types when in a module with a test-like name.
    This is needed for our opensource tests.
    
    PiperOrigin-RevId: 410418944

--- pytype/pytd/base_visitor.py
+++ pytype/pytd/base_visitor.py
@@ -1,5 +1,6 @@
 """Base class for visitors."""
 
+import re
 from typing import Any
 
 from pytype.pytd import pytd
@@ -177,7 +178,8 @@ class Visitor:
             # StrictType node, and pytd.printer.PrintVisitor has a visitor to
             # handle it.
             visit_all = True
-          elif cls.__module__ == "__main__":
+          elif (cls.__module__ == "__main__" or
+                re.fullmatch(r".*(_test|test_[^\.]+)", cls.__module__)):
             # We are running test code or something else that is defining its
             # own pytd nodes directly in a top-level python file.
             visit_all = True
