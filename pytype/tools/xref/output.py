bfd37d269b004408e21892f93ab5fef4d8228da2
mdemello <mdemello@google.com>
2022-02-28 19:57:31 -0800

    Restore output.unpack(); kythe was using it.
    PiperOrigin-RevId: 431586142

--- pytype/tools/xref/output.py
+++ pytype/tools/xref/output.py
@@ -4,6 +4,21 @@ import dataclasses
 import json
 
 
+def unpack(obj):
+  """Recursively expand namedtuples into dicts."""
+
+  if hasattr(obj, "_asdict"):
+    return {k: unpack(v) for k, v in obj._asdict().items()}
+  elif isinstance(obj, dict):
+    return {k: unpack(v) for k, v in obj.items()}
+  elif isinstance(obj, list):
+    return [unpack(v) for v in obj]
+  elif isinstance(obj, tuple):
+    return tuple(unpack(v) for v in obj)
+  else:
+    return obj
+
+
 def json_kythe_graph(kythe_graph):
   """Generate kythe entries."""
 
