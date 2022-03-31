d96a9c34a22f80dc032be9cd4327e6815c8cbe0e
rechen <rechen@google.com>
2022-03-30 18:16:38 -0700

    Default --trace-all-classes to True.
    The blocklist is empty.
    
    PiperOrigin-RevId: 438445804

--- pytype/config.py
+++ pytype/config.py
@@ -156,7 +156,7 @@ FEATURE_FLAGS = [
      "Enable exhaustive checking of function parameter types."),
     ("--enable-typed-dicts", False,
      "Enable support for TypedDicts."),
-    ("--trace-all-classes", False,
+    ("--trace-all-classes", True,
      "Trace class creation for better type analysis of class bodies."),
     ("--strict-primitive-comparisons", False,
      "Emit errors for comparisons between incompatible primitive types."),
