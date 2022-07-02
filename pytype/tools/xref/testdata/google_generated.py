78d3254853f8d9db89947861ab01f17136cdabcb
zwzhao <zwzhao@google.com>
2022-07-01 19:07:20 -0700

    Internal change
    PiperOrigin-RevId: 458584724

---
+++ pytype/tools/xref/testdata/google_generated.py
@@ -0,0 +1,32 @@
+# pylint: skip-file
+
+#- @h defines/binding NodeH
+h = 100794
+
+#- SourceAnchor imputes NodeH
+#- SourceAnchor.node/kind anchor
+#- SourceAnchor./kythe/loc/start 100
+#- SourceAnchor./kythe/loc/end 101
+
+'''
+Kythe Indexing Meta DebugString:
+  type: KYTHE0
+  meta {
+    type: ANCHOR_ANCHOR
+    edge: "/kythe/edge/imputes"
+    source_vname {
+      signature: "a"
+      corpus: "b"
+      root: "c"
+      path: "d"
+      language: "e"
+    }
+    source_begin: 100
+    source_end: 101
+    target_begin: 49
+    target_end: 50
+  }
+'''
+
+# Kythe Indexing Meta:
+# EjBQMkgxQGU4ZDIPCgFhEgFiGgFjIgFkKgFlEhMva3l0aGUvZWRnZS9pbXB1dGVzCAMIAQ==
