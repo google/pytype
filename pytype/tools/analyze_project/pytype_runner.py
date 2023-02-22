53db9310cca269d6c65ebaaf92f560879b4202f8
ngam <67342040+ngam@users.noreply.github.com>
2023-02-21 22:53:58 -0500

    fix ninja importability when python-ninja isn't available

--- pytype/tools/analyze_project/pytype_runner.py
+++ pytype/tools/analyze_project/pytype_runner.py
@@ -1,5 +1,6 @@
 """Use pytype to analyze and infer types for an entire project."""
 
+import importlib
 import collections
 import itertools
 import logging
@@ -46,7 +47,8 @@ def _get_executable(binary, module=None):
               path_utils.abspath(path_utils.dirname(custom_bin)),
               'pytype-single')
       ])
-  if sys.executable is not None:
+  importable = importlib.util.find_spec(module or binary)
+  if sys.executable is not None and importable is not None:
     return [sys.executable, '-m', module or binary]
   else:
     return [binary]
