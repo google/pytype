22d28147165e0653ab2df23f5ffb901ae4959a46
mdemello <mdemello@google.com>
2023-06-30 09:28:33 -0700

    Fix merge_pyi tests after updating libcst.
    The new version of libcst preserves formatting while merging, rather than
    autoformat the merged signatures.
    
    PiperOrigin-RevId: 544676325

--- pytype/tools/merge_pyi/test_data/parse_error.py
+++ pytype/tools/merge_pyi/test_data/parse_error.py
@@ -1,11 +1,2 @@
-# syntax error doesn't get detected by 2to3
-
+# This will raise a MergeError
 def f(*): pass
-
-
-# Gets parsed as:
-# Node(funcdef,
-#      [Leaf(1, 'def'), Leaf(1, 'f'),
-#       Node(parameters, [Leaf(7, '('), Leaf(16, '*'), Leaf(8, ')')]),
-#       Leaf(11, ':'),
-#       Node(simple_stmt, [Leaf(1, 'pass')])])
