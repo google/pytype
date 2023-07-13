fc3d1cb4ff5695d21609a0785157e89b46885ec4
rechen <rechen@google.com>
2023-07-13 16:21:23 -0700

    Cleanup: de-duplicate ways of getting a class's annotations_dict.
    We had two ways of accessing annotations_dict. I removed the property and left
    the abstract_utils helper function because the function makes it clearer that
    retrieving the dict is a non-trivial amount of work.
    
    PiperOrigin-RevId: 547958787

--- pytype/abstract/class_mixin.py
+++ pytype/abstract/class_mixin.py
@@ -302,13 +302,6 @@ class Class(metaclass=mixin.MixinMeta):  # pylint: disable=undefined-variable
     return False
 
   @property
-  def annotations_dict(self):
-    ann = self.members.get("__annotations__")
-    if ann:
-      return abstract_utils.get_atomic_value(ann)
-    return None
-
-  @property
   def is_abstract(self):
     return ((self._has_explicit_abcmeta() or self._has_implicit_abcmeta()) and
             bool(self.abstract_methods))
@@ -331,7 +324,7 @@ class Class(metaclass=mixin.MixinMeta):  # pylint: disable=undefined-variable
             self.__class__.__name__ == "TypedDictClass")
 
   def get_annotated_local(self, name):
-    ann = self.annotations_dict
+    ann = abstract_utils.get_annotations_dict(self.members)
     return ann and ann.annotated_locals.get(name)
 
   def _get_inherited_metaclass(self):
