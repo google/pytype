google/pytype

commit 569242009764214fa450c4b90b47505dd4636247

Fix crash caused by assuming parameter names are strings.
* Fixes a crash in the pytd printer.
* Fixes a false positive [container-type-mismatch] that is revealed once the
  crash is fixed.

PiperOrigin-RevId: 582743030

pytype/abstract/_pytd_function.py
@@ -260,40 +260,57 @@ class PyTDFunction(_function_base.Function):
         name = mutation.name
         values = mutation.value
         if obj.from_annotation:
-          params = obj.get_instance_type_parameter(name)
-          ps = {v for v in params.data if should_check(v)}
-          if ps:
-            filtered_values = self.ctx.program.NewVariable()
-            # check if the container type is being broadened.
-            new = []
-            short_name = name.rsplit(".", 1)[-1]
-            for b in values.bindings:
-              if not should_check(b.data) or b.data in ps:
-                filtered_values.PasteBinding(b)
-                continue
-              new_view = datatypes.AccessTrackingDict.merge(
-                  combined_view, view, {values: b})
-              if not compatible_with(values, ps, new_view):
-                combination = [b]
-                bad_param = b.data.get_instance_type_parameter(short_name)
-                if bad_param in new_view:
-                  combination.append(new_view[bad_param])
-                if not node.HasCombination(combination):
-                  # Since HasCombination is expensive, we don't use it to
-                  # pre-filter bindings, but once we think we have an error, we
-                  # should double-check that the binding is actually visible. We
-                  # also drop non-visible bindings from filtered_values.
-                  continue
-                filtered_values.PasteBinding(b)
-                new.append(b.data)
-            # By updating filtered_mutations only when ps is non-empty, we
-            # filter out mutations to parameters with type Any.
-            filtered_mutations.append(
-                function.Mutation(obj, name, filtered_values))
-            if new:
-              errors[obj][short_name] = (params, values, obj.from_annotation)
+          # We should check for parameter mismatches only if the class is
+          # generic. Consider:
+          #   class A(tuple[int, int]): ...
+          #   class B(tuple): ...
+          # Although pytype computes mutations for tuple.__new__ for both
+          # classes, the second implicitly inherits from tuple[Any, ...], so
+          # there are no restrictions on the container contents.
+          check_params = False
+          for cls in obj.cls.mro:
+            if isinstance(cls, _classes.ParameterizedClass):
+              check_params = True
+              break
+            elif cls.template:
+              break
         else:
+          check_params = False
+        if not check_params:
           filtered_mutations.append(function.Mutation(obj, name, values))
+          continue
+        params = obj.get_instance_type_parameter(name)
+        ps = {v for v in params.data if should_check(v)}
+        if ps:
+          filtered_values = self.ctx.program.NewVariable()
+          # check if the container type is being broadened.
+          new = []
+          short_name = name.rsplit(".", 1)[-1]
+          for b in values.bindings:
+            if not should_check(b.data) or b.data in ps:
+              filtered_values.PasteBinding(b)
+              continue
+            new_view = datatypes.AccessTrackingDict.merge(
+                combined_view, view, {values: b})
+            if not compatible_with(values, ps, new_view):
+              combination = [b]
+              bad_param = b.data.get_instance_type_parameter(short_name)
+              if bad_param in new_view:
+                combination.append(new_view[bad_param])
+              if not node.HasCombination(combination):
+                # Since HasCombination is expensive, we don't use it to
+                # pre-filter bindings, but once we think we have an error, we
+                # should double-check that the binding is actually visible. We
+                # also drop non-visible bindings from filtered_values.
+                continue
+              filtered_values.PasteBinding(b)
+              new.append(b.data)
+          # By updating filtered_mutations only when ps is non-empty, we
+          # filter out mutations to parameters with type Any.
+          filtered_mutations.append(
+              function.Mutation(obj, name, filtered_values))
+          if new:
+            errors[obj][short_name] = (params, values, obj.from_annotation)
 
       all_mutations = filtered_mutations
 

pytype/pytd/printer.py
@@ -723,7 +723,7 @@ class PrintVisitor(base_visitor.Visitor):
         assert isinstance(param, (pytd.NothingType, pytd.TypeParameter)), param
       parameters = ("...",) + parameters[1:]
     return (self.MaybeCapitalize(node.base_type) +
-            "[" + ", ".join(parameters) + "]")
+            "[" + ", ".join(str(p) for p in parameters) + "]")
 
   def VisitCallableType(self, node):
     typ = self.MaybeCapitalize(node.base_type)

pytype/tests/test_tuple2.py
@@ -253,6 +253,33 @@ class TupleTestPython3Feature(test_base.BaseTest):
       f((0, 0))  # ok
     """)
 
+  def test_imported_tuple_subclass_with_new(self):
+    with self.DepTree([("foo.pyi", """
+      from typing import TypeVar
+      _T = TypeVar('_T', bound=C)
+      class C(tuple):
+        def __new__(
+            cls: type[_T], x: str | list[tuple[int, tuple[int, int]]]
+        ) -> _T: ...
+    """)]):
+      ty = self.Infer("""
+        import foo
+        class A:
+          def __init__(self, c: foo.C):
+            self.c = foo.C('+'.join([f'{x}{y}' for x, y in c]))
+        class B:
+          def __init__(self, c: foo.C = foo.C([(0, (1, 2))])):
+            pass
+      """)
+      self.assertTypesMatchPytd(ty, """
+        import foo
+        class A:
+          c: foo.C
+          def __init__(self, c: foo.C) -> None: ...
+        class B:
+          def __init__(self, c: foo.C = ...) -> None: ...
+      """)
+
 
 if __name__ == "__main__":
   test_base.main()

commit 41556a8fa25da199e10c45d6233e477ff0a81eb1

Fix container mutation bug.
Fixes bug that I introduced in
https://github.com/google/pytype/commit/569242009764214fa450c4b90b47505dd4636247
by unconditionally performing mutations when we don't need to type-check
container contents. The type-checking code was also doing some Any filtering
that we always need when a container is type-annotated.

Fixes https://github.com/google/pytype/issues/1534.

PiperOrigin-RevId: 586119801

pytype/abstract/_pytd_function.py
@@ -259,58 +259,56 @@ class PyTDFunction(_function_base.Function):
         obj = mutation.instance
         name = mutation.name
         values = mutation.value
-        if obj.from_annotation:
-          # We should check for parameter mismatches only if the class is
-          # generic. Consider:
-          #   class A(tuple[int, int]): ...
-          #   class B(tuple): ...
-          # Although pytype computes mutations for tuple.__new__ for both
-          # classes, the second implicitly inherits from tuple[Any, ...], so
-          # there are no restrictions on the container contents.
-          check_params = False
-          for cls in obj.cls.mro:
-            if isinstance(cls, _classes.ParameterizedClass):
-              check_params = True
-              break
-            elif cls.template:
-              break
-        else:
-          check_params = False
-        if not check_params:
+        if not obj.from_annotation:
           filtered_mutations.append(function.Mutation(obj, name, values))
           continue
         params = obj.get_instance_type_parameter(name)
         ps = {v for v in params.data if should_check(v)}
-        if ps:
-          filtered_values = self.ctx.program.NewVariable()
-          # check if the container type is being broadened.
-          new = []
-          short_name = name.rsplit(".", 1)[-1]
-          for b in values.bindings:
-            if not should_check(b.data) or b.data in ps:
-              filtered_values.PasteBinding(b)
-              continue
-            new_view = datatypes.AccessTrackingDict.merge(
-                combined_view, view, {values: b})
-            if not compatible_with(values, ps, new_view):
-              combination = [b]
-              bad_param = b.data.get_instance_type_parameter(short_name)
-              if bad_param in new_view:
-                combination.append(new_view[bad_param])
-              if not node.HasCombination(combination):
-                # Since HasCombination is expensive, we don't use it to
-                # pre-filter bindings, but once we think we have an error, we
-                # should double-check that the binding is actually visible. We
-                # also drop non-visible bindings from filtered_values.
-                continue
-              filtered_values.PasteBinding(b)
-              new.append(b.data)
+        if not ps:
           # By updating filtered_mutations only when ps is non-empty, we
           # filter out mutations to parameters with type Any.
-          filtered_mutations.append(
-              function.Mutation(obj, name, filtered_values))
-          if new:
-            errors[obj][short_name] = (params, values, obj.from_annotation)
+          continue
+        # We should check for parameter mismatches only if the class is generic.
+        # Consider:
+        #   class A(tuple[int, int]): ...
+        #   class B(tuple): ...
+        # Although pytype computes mutations for tuple.__new__ for both classes,
+        # the second implicitly inherits from tuple[Any, ...], so there are no
+        # restrictions on the container contents.
+        check_params = False
+        for cls in obj.cls.mro:
+          if isinstance(cls, _classes.ParameterizedClass):
+            check_params = True
+            break
+          elif cls.template:
+            break
+        filtered_values = self.ctx.program.NewVariable()
+        # check if the container type is being broadened.
+        new = []
+        short_name = name.rsplit(".", 1)[-1]
+        for b in values.bindings:
+          if not check_params or not should_check(b.data) or b.data in ps:
+            filtered_values.PasteBinding(b)
+            continue
+          new_view = datatypes.AccessTrackingDict.merge(
+              combined_view, view, {values: b})
+          if not compatible_with(values, ps, new_view):
+            combination = [b]
+            bad_param = b.data.get_instance_type_parameter(short_name)
+            if bad_param in new_view:
+              combination.append(new_view[bad_param])
+            if not node.HasCombination(combination):
+              # Since HasCombination is expensive, we don't use it to
+              # pre-filter bindings, but once we think we have an error, we
+              # should double-check that the binding is actually visible. We
+              # also drop non-visible bindings from filtered_values.
+              continue
+            filtered_values.PasteBinding(b)
+            new.append(b.data)
+        filtered_mutations.append(
+            function.Mutation(obj, name, filtered_values))
+        if new:
+          errors[obj][short_name] = (params, values, obj.from_annotation)
 
       all_mutations = filtered_mutations
 

pytype/tests/test_dict2.py
@@ -136,6 +136,14 @@ class DictTest(test_base.BaseTest):
         kwargs.update(a=0, b='1')
     """)
 
+  def test_any(self):
+    self.Check("""
+      from typing import Dict
+      def do_something(d: Dict) -> None:
+        d['a'] = 0
+        d['b'] = d['b'].split(':')
+    """)
+
 
 if __name__ == "__main__":
   test_base.main()