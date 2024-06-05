4841b45dafc1dd82a228660573c674fcd15c8e9b
Hana Joo <hanajoo@google.com>
2024-06-05 01:35:04 -0700

    internal change.
    PiperOrigin-RevId: 640440025

---
+++ pytype/build_scripts/release.py
@@ -0,0 +1,122 @@
+#! /usr/bin/python
+"""A script to push a new Pytype release to PyPI.
+
+This script assumes that you have twine installed. The easiest way to run this
+script is to run from inside of a virtualenv after "pip" installing "twine".
+Also, this virtualenv should not have pytype installed already.
+
+USAGE:
+
+$> python release.py --mode=<TEST|RELEASE>
+
+If mode is "TEST", then the release will be pushed to testpypi. If the mode is
+"RELEASE", then the release is pushed to pypi.
+"""
+
+import argparse
+import os
+import shutil
+import sys
+
+import build_utils
+
+TEST_MODE = "TEST"
+RELEASE_MODE = "RELEASE"
+
+
+class ReleaseError(Exception):
+
+  def __init__(self, msg):
+    super().__init__()
+    self.msg = msg
+
+
+def parse_args():
+  """Parse and return the command line args."""
+  allowed_modes = (TEST_MODE, RELEASE_MODE)
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      "-m", "--mode", type=str, default=f"{TEST_MODE}",
+      help="Specify if the release should be uploaded to pypi or testpyi. Can "
+           "take a value of %s or %s" % allowed_modes)
+  args = parser.parse_args()
+  if args.mode not in allowed_modes:
+    sys.exit(f"Invalid --mode option. Should be one of {allowed_modes}")
+  return args
+
+
+def verify_no_pytype_installation_exists():
+  try:
+    import pytype as _  # pylint: disable=g-import-not-at-top
+  except ImportError:
+    return  # This is what we want - that Pytype does not already exist.
+  sys.exit("ERROR: Pytype installation detected; Run this script from inside "
+           "a virtualenv without a pytype installation.")
+
+
+def verify_pypirc_exists():
+  pypirc_path = os.path.join(os.path.expanduser("~"), ".pypirc")
+  if not os.path.exists(pypirc_path):
+    sys.exit("ERROR: '.pypirc' file not found.")
+
+
+def check_if_version_is_ok():
+  """Prompt the user to confirm that the version in __version__.py is OK."""
+  sys.path.append(build_utils.PYTYPE_SRC_ROOT)
+  version_mod = __import__("pytype.__version__", fromlist=["pytype"])
+  response = input("Making a release with version %s; Continue? " %
+                   getattr(version_mod, "__version__"))
+  if response not in ["y", "Y", "yes", "YES"]:
+    sys.exit("Aborting release.")
+
+
+def upload_package(package_path, test=False):
+  twine_cmd = ["twine", "upload"]
+  if test:
+    twine_cmd.extend(["--repository", "testpypi"])
+  twine_cmd.append(os.path.join(package_path, "*"))
+  print(f"Uploading: {twine_cmd}")
+  returncode, stdout = build_utils.run_cmd(twine_cmd)
+  if returncode != 0:
+    raise ReleaseError(f"Package upload failed:\n{stdout}")
+
+
+class DistributionPackage:
+  """Context manager to build the pytype distribution package."""
+
+  def __enter__(self):
+    sdist_cmd = ["python", "setup.py", "sdist"]
+    print(f"Creating distribution package: {sdist_cmd}\n")
+    returncode, stdout = build_utils.run_cmd(sdist_cmd)
+    if returncode != 0:
+      raise ReleaseError(f"Running {sdist_cmd} failed:\n{stdout}")
+    # The sdist command creates the distribution package in a directory
+    # named "dist"
+    self.dist_path = os.path.join(build_utils.PYTYPE_SRC_ROOT, "dist")
+    return self.dist_path
+
+  def __exit__(self, exc_type, exc_val, exc_tb):
+    print("Deleting the distribution directory ...\n")
+    shutil.rmtree(self.dist_path)
+    print("Deleting the metadata directory ...\n")
+    shutil.rmtree(os.path.join(build_utils.PYTYPE_SRC_ROOT, "pytype.egg-info"))
+    return False
+
+
+def main():
+  args = parse_args()
+
+  verify_no_pytype_installation_exists()
+  check_if_version_is_ok()
+  verify_pypirc_exists()
+
+  try:
+    with DistributionPackage() as pkg_path:
+      upload_package(pkg_path, args.mode == TEST_MODE)
+  except ReleaseError as error:
+    sys.exit(f">>> Release Failed <<<\n{error.msg}")
+  print("!!! Release Successful !!!\n")
+
+
+if __name__ == "__main__":
+  main()
---
+++ pytype/build_scripts/run_tests.py
@@ -0,0 +1,56 @@
+#! /usr/bin/python
+"""Script to run PyType tests.
+
+Usage:
+
+$> python run_tests.py [TARGET] [TARGET] ...
+
+A TARGET is a fully qualified name of a test target within the PyType
+source tree. If no target is specified, all test targets listed in the
+CMake files will be run.
+"""
+
+import argparse
+import sys
+
+import build_utils
+
+def parse_args():
+  """Parse the args to this script and return them."""
+  parser = argparse.ArgumentParser()
+  parser.add_argument("targets", metavar="TARGET", nargs="*",
+                      help="List of test targets to run.")
+  parser.add_argument("--fail_fast", "-f", action="store_true", default=False,
+                      help="Fail as soon as one build target fails.")
+  parser.add_argument("--debug", "-d", action="store_true", default=False,
+                      help="Build targets in the debug mode.")
+  parser.add_argument("--verbose", "-v", action="store_true", default=False,
+                      help="Print failing test logs to stderr.")
+  args = parser.parse_args()
+  for target in args.targets:
+    if "." in target:
+      _, target_name = target.rsplit(".", 1)
+    else:
+      target_name = target
+    if not (target_name.startswith("test_") or target_name.endswith("_test")):
+      sys.exit(f"The name '{target}' is not a valid test target name.")
+  return args
+
+
+def main():
+  opts = parse_args()
+  targets = opts.targets or ["test_all"]
+  if not build_utils.run_cmake(log_output=True, debug_build=opts.debug):
+    sys.exit(1)
+  fail_collector = build_utils.FailCollector()
+  print("Running tests (build steps will be executed as required) ...\n")
+  if not build_utils.run_ninja(
+      targets, fail_collector, opts.fail_fast, opts.verbose):
+    fail_collector.print_report(opts.verbose)
+    sys.exit(1)
+  print("!!! All tests passed !!!\n"
+        "Some tests might not have been run because they were already passing.")
+
+
+if __name__ == "__main__":
+  main()
