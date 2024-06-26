2ed59a069a48ed2843b9a2ca796e5c42ce32bbcb
Hana Joo <hanajoo@google.com>
2024-06-26 08:39:10 -0700

    Add a test for matching match-case statements, where the match-case of does not properly respect an existing attribute on a class.
    PiperOrigin-RevId: 646959959

---
+++ pytype/tests/test_match_case.py
@@ -0,0 +1,32 @@
+"""Tests for the match-case statement newly introduced in python in 3.10."""
+
+from pytype.tests import test_base
+from pytype.tests import test_utils
+
+
+@test_utils.skipBeforePy(
+    (3, 10), "match case statements only introduced in python 3.10"
+)
+class MatchCaseTest(test_base.BaseTest):
+  """Tests for matching match-case statements."""
+
+  def test_match_case_with_classes(self):
+    self.CheckWithErrors("""
+      import dataclasses
+      @dataclasses.dataclass(frozen=True)
+      class A:
+        a: str
+      @dataclasses.dataclass(frozen=True)
+      class B:
+        b: int
+      def test(x: bool) -> A | B:
+        return A('a')
+      match test(True):
+        case A(a): # Currently this case statements is not matched.
+          # TODO(b/346840739) - add suppression for unsupported-operands
+          print(a + 1)
+    """)
+
+
+if __name__ == "__main__":
+  test_base.main()
