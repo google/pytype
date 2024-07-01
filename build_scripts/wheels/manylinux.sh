8c88487516d63e3bddee45fda210d3daeaa6f7eb
Oleh Prypin <oprypin@google.com>
2024-07-01 05:18:30 -0700

    Switch away from "manylinux2014" image (EOL) for building wheels
    That image is based on CentOS 7 which is now EOL, and so CI is currently broken.
    
    Switch to the next oldest non-EOL Docker image "manylinux_2_28".
    
    https://github.com/pypa/manylinux/blo[]dac23b5ac8e8f5bb1b5acfebfaa49ac00a87126/README.rst
    
    PiperOrigin-RevId: 648333534

--- build_scripts/wheels/manylinux.sh
+++ build_scripts/wheels/manylinux.sh
@@ -8,10 +8,9 @@ if [[ -z "$PLAT" ]]; then
   exit 1
 fi
 
-yum install -y gettext-devel python3-devel # gettext is for flex
+dnf install -y ninja-build gettext-devel python3-devel # gettext is for flex
 
 # TODO: what should be the update cadence for these?
-NINJA_VERSION='1.7.2'
 BISON_VERSION='3.6'
 FLEX_VERSION='2.6.4'
 
@@ -20,21 +19,6 @@ untar() {
   tar -C "$1" -xzvf "${1}.tar.gz" --strip-components=1
   rm -vf "${1}.tar.gz"
 }
-
-# Install ninja/ninja-build (requires CMake)
-if [ `uname -m` == "aarch64" ]; then
-    yum install -y epel-release
-    yum-config-manager --enable epel
-    yum install -y ninja-build
-    ln -s /usr/bin/ninja-build /usr/bin/ninja
-else
-   curl -sSL \
-      -o ninja.zip \
-      "https://github.com/ninja-build/ninja/releases/download/v${NINJA_VERSION}/ninja-linux.zip"
-   unzip ninja.zip
-   mv ninja /usr/local/bin/
-   rm -vf ninja*
-fi
 
 TD="$(mktemp -d)"
 pushd "$TD" || exit 1
