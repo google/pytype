google/pytype

commit d02e721f07e9818fc626943d728f28b8b81aefc5

Treat setting fast local to null as a delete (for 3.12)
Python internally distinguishes between locals (variables stored in frame.f_locals) and "fast locals" (variables stored essentially at the bottom of the stack).

This typically doesn't matter because before locals are accessed, fast locals are copied into locals using `PyFrame_FastToLocals`[1].

But it _does_ matter when setting fast locals to NULL. Because setting a fast local to null means deleting the local.[2]

Not sure why this hasn't been an issues n 3.11 already. But 3.12 uses this in a few more places. E.g. list comprehensions are now inlined and the iteration variable is a fast local and set to null in the end. This change is needed to tell Pytype that the iteration variable does not exist anymore and should not be included in .pyi files, etc.

[1]: https://github.com/python/cpython/blo[]d1f15712d243e03e50332abb86ff4c121814aa5/Objects/frameobject.c#L1375
[2]: https://github.com/python/cpython/blo[]d1f15712d243e03e50332abb86ff4c121814aa5/Objects/frameobject.c#L1255-L1256

Failing tests: before=155, after=135

PiperOrigin-RevId: 657944178

pytype/vm.py
@@ -1695,7 +1695,11 @@ class VirtualMachine:
         val = self._name_error_or_late_annotation(state, name).to_variable(
             state.node
         )
-    vm_utils.check_for_deleted(state, name, val, self.ctx)
+    if on_uninitialized == _UninitializedBehavior.PUSH_NULL:
+      if any(isinstance(x, abstract.Deleted) for x in val.Data(state.node)):
+        val = abstract.Null(self.ctx).to_variable(state.node)
+    else:
+      vm_utils.check_for_deleted(state, name, val, self.ctx)
     self.trace_opcode(op, name, val)
     return state.push(val)
 
@@ -1710,12 +1714,19 @@ class VirtualMachine:
   def byte_LOAD_FAST_AND_CLEAR(self, state, op):
     name = op.argval
     state = self._load_fast(state, op, name, _UninitializedBehavior.PUSH_NULL)
-    null = abstract.Null(self.ctx).to_variable(state.node)
-    return self._store_value(state, name, null, local=True)
+    # According to the docs, we need to set the value to NULL. Since this is
+    # accessing "fast locals", setting to NULL is equalivent to deleting the
+    # value in f_locals.
+    return self._del_name(op, state, name, local=True)
 
   def byte_STORE_FAST(self, state, op):
     name = op.argval
-    return self._pop_and_store(state, op, name, local=True)
+    if isinstance(state.top().data[0], abstract.Null):
+      # Storing NULL in a "fast local" is equalivent to deleting the value in
+      # f_locals.
+      return self._del_name(op, state.pop_and_discard(), name, local=True)
+    else:
+      return self._pop_and_store(state, op, name, local=True)
 
   def byte_DELETE_FAST(self, state, op):
     name = op.argval

pytype/tracer_vm.py
@@ -641,6 +641,7 @@ class CallTracer(vm.VirtualMachine):
         name in abstract_utils.TOP_LEVEL_IGNORE
         or self._is_typing_member(name, var)
         or self._is_future_feature(name, var)
+        or any(isinstance(v, abstract.Deleted) for v in var.data)
     )
 
   def pytd_for_types(self, defs):

commit d269493460cf30eefca544437603ae66c8e07ca9

Fix crash in STORE_FAST if STACK[-1] has no bindings
Introduced in https://github.com/google/pytype/commit/d02e721f07e9818fc626943d728f28b8b81aefc5. We need to check if there are any bindings first.

PiperOrigin-RevId: 658722857

pytype/vm.py
@@ -1724,7 +1724,8 @@ class VirtualMachine:
 
   def byte_STORE_FAST(self, state, op):
     name = op.argval
-    if isinstance(state.top().data[0], abstract.Null):
+    top = state.top()
+    if top.data and isinstance(top.data[0], abstract.Null):
       # Storing NULL in a "fast local" is equalivent to deleting the value in
       # f_locals.
       return self._del_name(op, state.pop_and_discard(), name, local=True)

pytype/tests/test_basic2.py
@@ -40,6 +40,21 @@ class TestExec(test_base.BaseTest):
     """,
     )
 
+  def test_store_fast_empty_var(self):
+    self.assertNoCrash(
+        self.Check,
+        """
+        def bar():
+          raise NotImplementedError
+        def foo():
+          try:
+            bar()
+          except () as e: # Emits STORE_FAST for `e` and STACK[-1] is empty var
+                          # (i.e. no bindings)
+            pass
+        """,
+    )
+
 
 if __name__ == "__main__":
   test_base.main()