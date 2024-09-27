google/pytype

commit 147f60b236d86645156fbd4d53b8a6d70a277a4a

Add type annotations for blocks
I'm working on a fix for block graph ordering for Python 3.12. Having type annotations here makes the code easier to understand and navigate.

PiperOrigin-RevId: 679544438

pytype/blocks/blocks.py
@@ -1,12 +1,12 @@
 """Functions for computing the execution order of bytecode."""
 
-from typing import Any, List, Optional, Tuple, Union, cast
-
+from typing import Any, Iterator, List, Optional, Sequence, Tuple, Union, cast
 from pycnite import bytecode as pyc_bytecode
 from pycnite import marshal as pyc_marshal
 import pycnite.types
 from pytype.pyc import opcodes
 from pytype.typegraph import cfg_utils
+from typing_extensions import Self
 
 STORE_OPCODES = (
     opcodes.STORE_NAME,
@@ -55,13 +55,13 @@ class Block:
       instruction in our code object.
   """
 
-  def __init__(self, code):
+  def __init__(self, code: Sequence[opcodes.Opcode]):
     self.id = code[0].index
     self.code = code
-    self.incoming = set()
-    self.outgoing = set()
+    self.incoming: set[Self] = set()
+    self.outgoing: set[Self] = set()
 
-  def connect_outgoing(self, target):
+  def connect_outgoing(self, target: Self):
     """Add an outgoing edge."""
     self.outgoing.add(target)
     target.incoming.add(self)
@@ -121,7 +121,12 @@ class OrderedCode:
   order: List[Block]
   python_version: Tuple[int, int]
 
-  def __init__(self, code, bytecode, order: List[Block]):
+  def __init__(
+      self,
+      code: pycnite.types.CodeTypeBase,
+      bytecode: list[opcodes.Opcode],
+      order: list[Block],
+  ):
     assert hasattr(code, "co_code")
     self.name = code.co_name
     self.filename = code.co_filename
@@ -169,13 +174,14 @@ class OrderedCode:
     return self.consts
 
   @property
-  def code_iter(self):
+  def code_iter(self) -> Iterator[opcodes.Opcode]:
     return (op for block in self.order for op in block)  # pylint: disable=g-complex-comprehension
 
   def get_first_opcode(self, skip_noop=False):
     for op in self.code_iter:
       if not skip_noop or not isinstance(op, _NOOP_OPCODES):
         return op
+    assert False, "OrderedCode should have at least one opcode"
 
   def has_opcode(self, op_type):
     return any(isinstance(op, op_type) for op in self.code_iter)
@@ -222,16 +228,16 @@ class BlockGraph:
   """CFG made up of ordered code blocks."""
 
   def __init__(self):
-    self.graph = {}
+    self.graph: dict[opcodes.Opcode, OrderedCode] = {}
 
-  def add(self, ordered_code):
+  def add(self, ordered_code: OrderedCode):
     self.graph[ordered_code.get_first_opcode()] = ordered_code
 
   def pretty_print(self):
     return str(self.graph)
 
 
-def add_pop_block_targets(bytecode):
+def add_pop_block_targets(bytecode: list[opcodes.Opcode]) -> None:
   """Modifies bytecode so that each POP_BLOCK has a block_target.
 
   This is to achieve better initial ordering of try/except and try/finally code.
@@ -309,7 +315,7 @@ def add_pop_block_targets(bytecode):
       todo.append((op.next, block_stack))
 
 
-def _split_bytecode(bytecode):
+def _split_bytecode(bytecode: list[opcodes.Opcode]) -> list[Block]:
   """Given a sequence of bytecodes, return basic blocks.
 
   This will split the code at "basic block boundaries". These occur at
@@ -340,7 +346,7 @@ def _split_bytecode(bytecode):
   return blocks
 
 
-def compute_order(bytecode):
+def compute_order(bytecode: list[opcodes.Opcode]) -> list[Block]:
   """Split bytecode into blocks and order the blocks.
 
   This builds an "ancestor first" ordering of the basic blocks of the bytecode.

pytype/pyc/opcodes.py
@@ -67,6 +67,7 @@ class Opcode:
     self.prev = None
     self.next = None
     self.target = None
+    self.block_target = None
     self.code = None  # If we have a CodeType or OrderedCode parent
     self.annotation = None
     self.folded = None  # elided by constant folding

pytype/typegraph/cfg_utils.py
@@ -2,6 +2,7 @@
 
 import collections
 import itertools
+from typing import Iterable, Protocol, Sequence, TypeVar
 
 
 # Limit on how many argument combinations we allow before aborting.
@@ -219,7 +220,16 @@ def walk_binding(binding, keep_binding=lambda _: True):
         bindings.extend(itertools.chain(*o.source_sets))
 
 
-def compute_predecessors(nodes):
+class PredecessorNode(Protocol):
+  outgoing: "Iterable[PredecessorNode]"
+
+
+_PredecessorNode = TypeVar("_PredecessorNode", bound=PredecessorNode)
+
+
+def compute_predecessors(
+    nodes: Iterable[_PredecessorNode],
+) -> dict[_PredecessorNode, set[_PredecessorNode]]:
   """Build a transitive closure.
 
   For a list of nodes, compute all the predecessors of each node.
@@ -261,7 +271,14 @@ def compute_predecessors(nodes):
   return predecessors
 
 
-def order_nodes(nodes):
+class OrderableNode(PredecessorNode, Protocol):
+  id: int
+
+
+_OrderableNode = TypeVar("_OrderableNode", bound=OrderableNode)
+
+
+def order_nodes(nodes: Sequence[_OrderableNode]) -> list[_OrderableNode]:
   """Build an ancestors first traversal of CFG nodes.
 
   This guarantees that at least one predecessor of a block is scheduled before
@@ -270,8 +287,8 @@ def order_nodes(nodes):
   process both the branches before that node).
 
   Args:
-    nodes: A list of nodes or blocks. They have two attributes: "incoming" and
-      "outgoing". Both are lists of other nodes.
+    nodes: A list of nodes or blocks. They have two attributes: "id" (an int to
+      enable deterministic sorting) and "outgoing" (a list of nodes).
 
   Returns:
     A list of nodes in the proper order.

commit 2b4b2963c129b38e8f1006f5cad846073876dbfe

Fix type error in rewrite/frame_test.py
FrameTestBase.run_block appends an element to Block().code. For some reason this error only surfaces in 3.12 and we don't run type checking on that file on GitHub.

Error introduced by https://github.com/google/pytype/commit/147f60b236d86645156fbd4d53b8a6d70a277a4a.

PiperOrigin-RevId: 679577463

pytype/blocks/blocks.py
@@ -1,6 +1,6 @@
 """Functions for computing the execution order of bytecode."""
 
-from typing import Any, Iterator, List, Optional, Sequence, Tuple, Union, cast
+from typing import Any, Iterator, List, Optional, Tuple, Union, cast
 from pycnite import bytecode as pyc_bytecode
 from pycnite import marshal as pyc_marshal
 import pycnite.types
@@ -55,7 +55,7 @@ class Block:
       instruction in our code object.
   """
 
-  def __init__(self, code: Sequence[opcodes.Opcode]):
+  def __init__(self, code: list[opcodes.Opcode]):
     self.id = code[0].index
     self.code = code
     self.incoming: set[Self] = set()

pytype/rewrite/tests/test_utils.py
@@ -41,7 +41,7 @@ class PytdTestBase(parser_test_base.ParserTest):
 
 class FakeOrderedCode(i4t.ProductionType[blocks.OrderedCode]):
 
-  def __init__(self, ops: Sequence[Sequence[opcodes.Opcode]], consts=()):
+  def __init__(self, ops: Sequence[list[opcodes.Opcode]], consts=()):
     self.order = [blocks.Block(block_ops) for block_ops in ops]
     self.consts = consts
 