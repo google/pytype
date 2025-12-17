2d87a2af86ba22c7f21144560a41e6e7eae734f5
pytype authors <no-reply@google.com>
2025-12-17 10:28:01 -0800

    deleting kythe indexer for pytype - it has not been used or maintained for years
    PiperOrigin-RevId: 845824363

--- pytype/tools/xref/testdata/builtins.py
+++
@@ -1,7 +0,0 @@
-# pylint: skip-file
-
-a = "hello"
-#- @split ref vname("module.str.split", _, _, "pytd:builtins", _)
-b = a.split('.')
-#- @reverse ref vname("module.list.reverse", _, _, "pytd:builtins", _)
-c = b.reverse()
